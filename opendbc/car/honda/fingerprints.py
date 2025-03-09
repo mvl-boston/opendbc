1207ec8f6005b4b6ceb3ab7615fc5cc02a558c8f
vanillagorillaa <31773928+vanillagorillaa@users.noreply.github.com>
2025-03-08 18:47:35 -0600

    fix
    (cherry picked from commit 15485ae0a776feaf92d5d8adb676fd58f39fbc77)

--- opendbc/car/honda/fingerprints.py
+++ opendbc/car/honda/fingerprints.py
@@ -1259,8 +1259,16 @@ FW_VERSIONS = {
       b'39990-T38-A040\x00\x00',
       b'39990-T38-A140\x00\x00',
     ],
-    (Ecu.fwdCamera, 0x18dab5f1, None): [
-      b'8S102-TGV-A030\x00\x00',
+    (Ecu.gateway, 0x18daeff1, None): [
+      b'38897-3S5-A110\x00\x00',
+      b'38897-3S5-A210\x00\x00',
+    ],
+    (Ecu.srs, 0x18da53f1, None): [
+      b'77959-3S5-A920\x00\x00',
+    ],
+    (Ecu.vsa, 0x18da28f1, None): [
+      b'57114-3S5-CA30\x00\x00',
+      b'57114-3S5-CB30\x00\x00',
     ],
     (Ecu.fwdRadar, 0x18dab0f1, None): [
       b'8S102-3S5-AA10\x00\x00',
