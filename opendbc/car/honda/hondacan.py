c5eaafda06a354e8b5aefd308958ddcdeaed88da
mvl-boston <manuel@alumni.virginia.edu>
2025-03-28 00:57:29 -0400

    Try typofix

--- opendbc/car/honda/hondacan.py
+++ opendbc/car/honda/hondacan.py
@@ -188,11 +188,10 @@ def create_ui_commands(packer, CAN, CP, enabled, pcm_speed, hud, is_metric, acc_
     # car likely needs to see LKAS_PROBLEM fall within a specific time frame, so forward from camera
     lkas_hud_values['LKAS_PROBLEM'] = lkas_hud['LKAS_PROBLEM']
     if not enabled:
-      hybrid_control_values = {
-        CURRENT_SPEED: hybrid_control['CURRENT_SPEED'],
-        TARGET_SPEED: hybrid_control['TARGET_SPEED'],
-        CONTROL_SIGNALS: hybrid_control['CONTROL_SIGNALS']
-      }
+      hybrid_control_values = {}
+      hybrid_control_values['CURRENT_SPEED'] = hybrid_control['CURRENT_SPEED']
+      hybrid_control_values['TARGET_SPEED'] = hybrid_control['TARGET_SPEED']
+      hybrid_control_values['CONTROL_SIGNALS'] = hybrid_control['CONTROL_SIGNALS']
       commands.append(packer.make_can_msg("HYBRID_CONTROL", CAN.lkas, hybrid_control_values))
 
   if not (CP.flags & HondaFlags.BOSCH_EXT_HUD):
