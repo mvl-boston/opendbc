37fd2d6d225a07cb9d171a199470bb07727e1479
mvl-boston <manuel@alumni.virginia.edu>
2025-03-28 00:04:33 -0400

    define hybrid control

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -278,6 +278,7 @@ class CarState(CarStateBase):
       self.stock_brake = cp_cam.vl["BRAKE_COMMAND"]
     if self.CP.carFingerprint in HONDA_BOSCH_RADARLESS:
       self.lkas_hud = cp_cam.vl["LKAS_HUD"]
+      self.hybrid_control = cp_cam.vl["HYBRID_CONTROL"]
 
     if self.CP.enableBsm:
       # BSM messages are on B-CAN, requires a panda forwarding B-CAN messages to CAN 0
