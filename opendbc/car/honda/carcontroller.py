874b99bb650bb85a271ac86c946c7e92f1a14458
mvl-boston <manuel@alumni.virginia.edu>
2025-04-04 22:05:34 -0400

    Trying hybrid control again

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -115,7 +115,6 @@ class CarController(CarControllerBase):
     self.gas = 0.0
     self.brake = 0.0
     self.last_torque = 0.0
-    self.brake_frames_sent = 0
 
   def update(self, CC, CS, now_nanos):
     actuators = CC.actuators
@@ -203,9 +202,8 @@ class CarController(CarControllerBase):
 
     else:
       # Send gas and brake commands.
-      if (self.frame % 2 == 0) or (self.brake_frames_sent < 10):
+      if self.frame % 2 == 0
         ts = self.frame * DT_CTRL
-        self.brake_frames_sent = min (99, self.brake_frames_sent + 1)
 
         if self.CP.carFingerprint in HONDA_BOSCH:
           self.accel = float(np.clip(accel, self.params.BOSCH_ACCEL_MIN, self.params.BOSCH_ACCEL_MAX))
@@ -233,7 +231,7 @@ class CarController(CarControllerBase):
       hud = HUDData(int(pcm_accel), int(round(hud_v_cruise)), hud_control.leadVisible,
                     hud_control.lanesVisible, fcw_display, acc_alert, steer_required, hud_control.leadDistanceBars)
       can_sends.extend(hondacan.create_ui_commands(self.packer, self.CAN, self.CP, CC.enabled, pcm_speed, \
-                                                   hud, CS.is_metric, CS.acc_hud, CS.lkas_hud ))
+                                                   hud, CS.is_metric, CS.acc_hud, CS.lkas_hud, CS.hybrid_control ))
 
       if self.CP.openpilotLongitudinalControl and self.CP.carFingerprint not in HONDA_BOSCH:
         self.speed = pcm_speed
