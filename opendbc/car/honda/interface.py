9aa25e074e5f9d140a3d447069483f1fafda0e8d
mvl-boston <manuel@alumni.virginia.edu>
2025-04-05 15:40:37 -0400

    Update interface.py

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -39,8 +39,8 @@ class CarInterface(CarInterfaceBase):
 
     if candidate in HONDA_CANFD_CAR:
       cfgs = [get_safety_config(structs.CarParams.SafetyModel.hondaBosch)]
-      if CAN.pt >= 4:
-        cfgs.insert(0, get_safety_config(structs.CarParams.SafetyModel.noOutput))
+      # if CAN.pt >= 4:
+      #  cfgs.insert(0, get_safety_config(structs.CarParams.SafetyModel.noOutput))
       ret.safetyConfigs = cfgs
       ret.radarUnavailable = True
       # Disable the radar and let openpilot control longitudinal
