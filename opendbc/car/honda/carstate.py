510601c7b31594f88148f0963286f358c195c487
mvl-boston <manuel@alumni.virginia.edu>
2025-04-08 18:14:18 -0400

    Update carstate.py

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -7,8 +7,8 @@ from opendbc.car import Bus, create_button_events, structs
 from opendbc.car.common.conversions import Conversions as CV
 from opendbc.car.honda.hondacan import CanBus, get_cruise_speed_conversion
 from opendbc.car.honda.values import CAR, DBC, STEER_THRESHOLD, HONDA_BOSCH, \
-                                                 HONDA_NIDEC_ALT_SCM_MESSAGES, HONDA_BOSCH_RADARLESS, HONDA_CANFD_CAR, \
-                                                 HondaFlags, CruiseButtons, CruiseSettings, GearShifter
+                                                 HONDA_NIDEC_ALT_SCM_MESSAGES, HONDA_BOSCH_RADARLESS, HondaFlags, \
+                                                 CruiseSettings, GearShifter # , CruiseButtons, HONDA_CANFD_CAR
 from opendbc.car.interfaces import CarStateBase
 
 TransmissionType = structs.CarParams.TransmissionType
