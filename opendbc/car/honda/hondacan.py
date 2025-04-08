8810c2fec0e4fec93f3fd81a81e3f8617ff6d898
mvl-boston <manuel@alumni.virginia.edu>
2025-04-08 18:19:16 -0400

    Update hondacan.py

--- opendbc/car/honda/hondacan.py
+++ opendbc/car/honda/hondacan.py
@@ -1,6 +1,6 @@
 from opendbc.car import CanBusBase
 from opendbc.car.common.conversions import Conversions as CV
-from opendbc.car.honda.values import HondaFlags, HONDA_BOSCH, HONDA_BOSCH_RADARLESS, HONDA_CANFD_CAR, CAR, CarControllerParams # , CruiseButtons
+from opendbc.car.honda.values import HondaFlags, HONDA_BOSCH, HONDA_BOSCH_RADARLESS, HONDA_CANFD_CAR, CAR, CarControllerParams #  , CruiseButtons
 
 # CAN bus layout with relay
 # 0 = ACC-CAN - radar side
