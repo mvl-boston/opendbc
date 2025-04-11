13005e75d73543cf2bd72735a66e45f4f082f40b
mvl-boston <manuel@alumni.virginia.edu>
2025-04-11 09:17:19 -0400

    Update safety_honda.h

--- opendbc/safety/safety/safety_honda.h
+++ opendbc/safety/safety/safety_honda.h
@@ -326,7 +326,7 @@ static safety_config honda_bosch_init(uint16_t param) {
   static CanMsg HONDA_RADARLESS_TX_MSGS[] = {{0xE4, 0, 5, true}, {0x296, 2, 4, false}, {0x33D, 0, 8, false}};  // Bosch radarless
   static CanMsg HONDA_RADARLESS_LONG_TX_MSGS[] = {{0xE4, 0, 5, true}, {0x33D, 0, 8, false}, {0x1C8, 0, 8, false}, {0x30C, 0, 8, false}};  // Bosch radarless w/ gas and brakes
   static CanMsg HONDA_CANFD_TX_MSGS[] = {{0xE4, 0, 5, true}, {0x33D, 0, 8, false}, {0xE5, 0, 8, false}, {0x296, 0, 4, false}};  // Bosch canfd
-  static CanMsg HONDA_CANFD_LONG_TX_MSGS[] = {{0xE4, 0, 5, true}, {0x1DF, 0, 8, false}, {0x1EF, 0, 8, false}, {0x30C, 0, 8, false}, {0x33D, 0, 8, false},  {0x39F, 0, 8, false}, {0x18DAB0F1, 1, 8, false}, {0xE4, 2, 5, true}, {0x1DF, 2, 8, false}, {0x1EF, 2, 8, false}, {0x30C, 2, 8, false}, {0x33D, 2, 8, false},  {0x39F, 2, 8, false}}; // replicating regular Bosch but try bus 0
+  static CanMsg HONDA_CANFD_LONG_TX_MSGS[] = {{0xE4, 0, 5, true}, {0x1DF, 0, 8, false}, {0x1EF, 0, 8, false}, {0x30C, 0, 8, false}, {0x33D, 0, 8, false},  {0x39F, 0, 8, false}, {0x18DAB0F1, 1, 8, false}, {0xE4, 1, 5, true}, {0x1DF, 1, 8, false}, {0x1EF, 1, 8, false}, {0x30C, 1, 8, false}, {0x33D, 1, 8, false},  {0x39F, 1, 8, false}}; // replicating regular Bosch but try bus 0
 
   const uint16_t HONDA_PARAM_ALT_BRAKE = 1;
   const uint16_t HONDA_PARAM_RADARLESS = 8;
