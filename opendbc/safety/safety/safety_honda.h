f7d43e97522a1670edd6194dda4b0acd3a74ffec
mvl-boston <manuel@alumni.virginia.edu>
2025-04-12 10:45:52 -0400

    Update safety_honda.h

--- opendbc/safety/safety/safety_honda.h
+++ opendbc/safety/safety/safety_honda.h
@@ -334,7 +334,7 @@ static safety_config honda_bosch_init(uint16_t param) {
                                               {0x33DA, 1, 5, .check_relay = true}, {0x33DB, 1, 8, .check_relay = true}, {0x39F, 1, 8, .check_relay = false},
                                               {0x18DAB0F1, 1, 8, .check_relay = false}};  // Bosch w/ gas and brakes
 
-  static CanMsg HONDA_CANFD_TX_MSGS[] = {{0xE4, 0, 5, .check_relay = true}, {0x33D, 0, 8,  .check_relay = false}, {0xE5, 0, 8, false}, {0x296, 0, 4,  .check_relay = false}};  // Bosch canfd
+  static CanMsg HONDA_CANFD_TX_MSGS[] = {{0xE4, 0, 5, .check_relay = true}, {0x33D, 0, 8, .check_relay = false}, {0xE5, 0, 8, .check_relay = false}, {0x296, 0, 4, .check_relay = false}};  // Bosch canfd
   
   static CanMsg HONDA_CANFD_LONG_TX_MSGS[] = {{0xE4, 0, 5,  .check_relay = true}, {0x1DF, 0, 8,  .check_relay = false}, {0x1EF, 0, 8,  .check_relay = false},
                                               {0x30C, 0, 8, .check_relay = false}, {0x33D, 0, 8, .check_relay = false},  {0x39F, 0, 8, .check_relay = false},
