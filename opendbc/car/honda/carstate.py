566436cb33ec0565538981f1120e0d97a3f5323a
mvl-boston <manuel@alumni.virginia.edu>
2025-04-25 03:09:50 -0400

    remove steering logic

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -251,9 +251,9 @@ class CarState(CarStateBase):
     ret.cruiseState.available = bool(cp.vl[self.main_on_sig_msg]["MAIN_ON"])
 
     # Adds low speed warning as some models disable cruise at various speeds, ignore warning under 3mph
-    if self.CP.carFingerprint in (CAR.HONDA_ODYSSEY_5G_MMR):
-      ret.lowSpeedAlert = ret.cruiseState.enabled and ret.vEgo >= 4 * CV.MPH_TO_MS and \
-        cp.vl["STEER_STATUS"]["STEER_CONTROL_ACTIVE"] == 0 and not ret.steeringPressed
+    #if self.CP.carFingerprint in (CAR.HONDA_ODYSSEY_5G_MMR):
+    #  ret.lowSpeedAlert = ret.cruiseState.enabled and ret.vEgo >= 4 * CV.MPH_TO_MS and \
+    #    cp.vl["STEER_STATUS"]["STEER_CONTROL_ACTIVE"] == 0 and not ret.steeringPressed
 
     # Gets rid of Pedal Grinding noise when brake is pressed at slow speeds for some models
     if self.CP.carFingerprint in (CAR.HONDA_PILOT, CAR.HONDA_RIDGELINE):
