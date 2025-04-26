a7bc6870707276da1dea86a5daff109c48eb0b01
mvl-boston <manuel@alumni.virginia.edu>
2025-04-26 15:40:43 -0400

    formatting

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -170,7 +170,7 @@ class CarController(CarControllerBase):
       self.last_cruise_speed = hud_control.setSpeed
 
     self.steer_cruise_override = False # temporarily remove slowsteer logic to isolate new safety debugging
-    
+
     # Send CAN commands
     can_sends = []
 
