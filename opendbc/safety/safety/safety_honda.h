c4d3c8b1e91db57f53e44f25e971ad83e11f8e91
mvl-boston <manuel@alumni.virginia.edu>
2025-04-26 21:14:42 -0400

    typo

--- opendbc/safety/safety/safety_honda.h
+++ opendbc/safety/safety/safety_honda.h
@@ -320,7 +320,7 @@ static safety_config honda_nidec_init(uint16_t param) {
 
 static safety_config honda_bosch_init(uint16_t param) {
   static CanMsg HONDA_BOSCH_TX_MSGS[] = {{0xE4, 0, 5, .check_relay = true}, {0xE5, 0, 8, .check_relay = false}, {0x296, 1, 4, .check_relay = false},
-                                         {0x33D, 0, 5, .check_relay = false}, {0x33DA, 0, 5, .check_relay = false}, {0x33DB, 0, 8, .check_relay = false}};  // Bosch A
+                                         {0x33D, 0, 5, .check_relay = false}, {0x33DA, 0, 5, .check_relay = false}, {0x33DB, 0, 8, .check_relay = false},  // Bosch A
                                          {0x296, 0, 4, .check_relay = false}, {0x33D, 0, 8, .check_relay = false}}; // add bosch supplemental and zero button bus for Bosch C / canfd
   static CanMsg HONDA_BOSCH_LONG_TX_MSGS[] = {{0xE4, 1, 5, .check_relay = true}, {0x1DF, 1, 8, .check_relay = true}, {0x1EF, 1, 8, .check_relay = false},
                                               {0x1FA, 1, 8, .check_relay = false}, {0x30C, 1, 8, .check_relay = false}, {0x33D, 1, 5, .check_relay = false},
