819373650509e2c80b5171f8e6f7ab2750b3bb8b
mvl-boston <manuel@alumni.virginia.edu>
2025-04-28 00:03:59 -0400

    formatting?

--- opendbc/safety/safety/safety_honda.h
+++ opendbc/safety/safety/safety_honda.h
@@ -438,7 +438,7 @@ static bool honda_nidec_fwd_hook(int bus_num, int addr) {
 static bool honda_bosch_fwd_hook(int bus_num, int addr) {
   bool block_msg = false;
 
-  if (bus_num == 2) && honda_bosch_canfd && honda_bosch_long {  // canfd was faulting when camera and comma attempted dual messages
+  if ( (bus_num == 2) && honda_bosch_canfd && honda_bosch_long ) {  // canfd was faulting when camera and comma attempted dual messages
     bool is_lkas_msg = (addr == 0xE4) || (addr == 0x33D);
     bool is_acc_msg = (addr == 0x1DF) || (addr == 0x1EF) || (addr == 0x30C)
     // block_msg = is_lkas_msg || is_acc_msg; - try ACC alone for now
