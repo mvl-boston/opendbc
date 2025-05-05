b696a6ca2c2d11bc545fabc64e06772e2e1f0d7a
mvl-boston <manuel@alumni.virginia.edu>
2025-05-04 23:32:45 -0400

    adding RLX

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -236,8 +236,14 @@ class CAR(Platforms):
     flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES,
   )
   ACURA_MDX_3G_HYBRID = HondaNidecPlatformConfig(
-    [HondaCarDocs("Acura MDX Hybrid 2018-20")],
+    [], # 2018-20 MDX Hybrid, # don't add to cardocs since serial steering board
     CarSpecs(mass=4486 * CV.LB_TO_KG, wheelbase=2.82, centerToFrontRatio=0.428, steerRatio=15.76, tireStiffnessFactor=0.444),  # acura spec, stiff from Pilot
+    radar_dbc_dict('acura_mdx_3G_hybrid'),
+    flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES | HondaFlags.NIDEC_HYBRID | HondaFlags.NIDEC_ALT_PCM_ACCEL,
+  )
+  ACURA_RLX_HYBRID = HondaNidecPlatformConfig(
+    [], # 2017 RLX Hybrid, don't add to cardocs since serial steering board
+    CarSpecs(mass=4359 * CV.LB_TO_KG, wheelbase=2.85, centerToFrontRatio=0.39, steerRatio=13.9, tireStiffnessFactor=0.8467),  # acura spec, stiff/ctf from Accord
     radar_dbc_dict('acura_mdx_3G_hybrid'),
     flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES | HondaFlags.NIDEC_HYBRID | HondaFlags.NIDEC_ALT_PCM_ACCEL,
   )
