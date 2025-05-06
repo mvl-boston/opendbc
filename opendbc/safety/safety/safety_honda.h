a575206355e494c13e13fa5c020c09c17873e9d4
mvl-boston <manuel@alumni.virginia.edu>
2025-05-06 14:26:27 -0400

    syntax

--- opendbc/safety/safety/safety_honda.h
+++ opendbc/safety/safety/safety_honda.h
@@ -423,7 +423,7 @@ static bool honda_nidec_fwd_hook(int bus_num, int addr) {
     
     // forwarded if stock AEB is active
     bool is_brake_msg = addr == 0x1FA;
-    block_msg = (is_brake_msg && !honda_fwd_brake) or is_lkas_msg;
+    block_msg = (is_brake_msg && !honda_fwd_brake) || is_lkas_msg;
   }
 
   return block_msg;
