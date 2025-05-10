4f02405687c7c0b4330bd1a4f232ce1d0888dde6
mvl-boston <manuel@alumni.virginia.edu>
2025-05-09 22:51:44 -0400

    typos

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -205,9 +205,9 @@ class CarController(CarControllerBase):
 
     # slow steering to maintain within honda limits
     steer_factor = 400 if (actuators.torque == 0 or CS.out.steeringPressed) else abs(self.params.STEER_MAX/max(abs(actuators.torque),abs(apply_torque)))
-    steer_max_accel = np.interp(steerfactor, [1.0, 3.0], [-2.0, 2.0])
-    steer_brake = min (atarget, steer_max_accel) - atarget
-    
+    steer_max_accel = np.interp(steer_factor, [1.0, 3.0], [-2.0, 2.0])
+    steer_brake = min (aTarget, steer_max_accel) - aTarget
+
     if not self.CP.openpilotLongitudinalControl:
       if self.frame % 2 == 0 and self.CP.carFingerprint not in HONDA_BOSCH_RADARLESS:  # radarless cars don't have supplemental message
         can_sends.append(hondacan.create_bosch_supplemental_1(self.packer, self.CAN))
