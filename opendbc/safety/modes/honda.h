25ef099eafb1c1756f93169726e746c8b4b1d293
mvl-boston <manuel@alumni.virginia.edu>
2025-05-20 20:54:48 -0400

    keep trying more steering options

--- opendbc/safety/modes/honda.h
+++ opendbc/safety/modes/honda.h
@@ -290,7 +290,8 @@ static bool honda_tx_hook(const CANPacket_t *to_send) {
 
 static safety_config honda_nidec_init(uint16_t param) {
 
-  static CanMsg HONDA_N_TX_MSGS[] = {{0xE4, 0, 5, .check_relay = true}, {0x194, 0, 4, .check_relay = true}, {0x1FA, 0, 8, .check_relay = false},
+  // static CanMsg HONDA_N_TX_MSGS[] = {{0xE4, 0, 5, .check_relay = true}, {0x194, 0, 4, .check_relay = true}, {0x1FA, 0, 8, .check_relay = false},
+  static CanMsg HONDA_N_TX_MSGS[] = {{0xE4, 0, 5, .check_relay = true}, {0x194, 2, 4, .check_relay = false}, {0x194, 0, 4, .check_relay = false}, {0x1FA, 0, 8, .check_relay = false},
                                      {0x30C, 0, 8, .check_relay = true}, {0x33D, 0, 5, .check_relay = true}};
   
   static CanMsg HONDA_N_ALT_STEER_TX_MSGS[] = {{0xE4, 0, 5, .check_relay = true}, {0x194, 2, 4, .check_relay = false}, {0x1FA, 0, 8, .check_relay = false},
