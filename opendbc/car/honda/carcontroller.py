d50559d70e75afed714b7f014d1b51c289f027bd
mvl-boston <manuel@alumni.virginia.edu>
2025-05-25 10:53:05 -0400

    fix calc_accel

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -119,6 +119,7 @@ class CarController(CarControllerBase):
     self.brake = 0.0
     self.last_torque = 0.0
     self.pitch = 0.0
+    self.calc_accel = 0.0
 
   def update(self, CC, CS, now_nanos):
     actuators = CC.actuators
@@ -203,10 +204,10 @@ class CarController(CarControllerBase):
       hill_brake = math.sin(self.pitch) * ACCELERATION_DUE_TO_GRAVITY
       hybrid_regen_brake = 0.25
 
-      calc_accel = accel + wind_brake_ms2 + hill_brake + hybrid_regen_brake
+      self.calc_accel = accel + wind_brake_ms2 + hill_brake + hybrid_regen_brake
       vfactor = np.interp(CS.out.vEgo, [0.0, 2.0, 100.0], [1000.0, 40.0, 40.0])
-      pcm_accel = int (np.clip(calc_accel * vfactor, 0, self.params.NIDEC_GAS_MAX) )
-      pcm_speed = max (0, CS.out.vEgo + (9.99 if calc_accel > 0 else -9.99 ) * CV.KPH_TO_MS)
+      pcm_accel = int (np.clip(self.calc_accel * vfactor, 0, self.params.NIDEC_GAS_MAX) )
+      pcm_speed = max (0, CS.out.vEgo + (9.99 if self.calc_accel > 0 else -9.99 ) * CV.KPH_TO_MS)
 # ----------------- test override gas end -------------------
     
     if not self.CP.openpilotLongitudinalControl:
@@ -241,7 +242,7 @@ class CarController(CarControllerBase):
 
 # ------------------ brake override begin
           vfactorBrake = np.interp(CS.out.vEgo, [0.0, 2.0, 100.0], [-75.0, -75.0, -75.0])
-          apply_brake = int(np.clip(calc_accel * vfactorBrake, 0, self.params.NIDEC_BRAKE_MAX - 1))         
+          apply_brake = int(np.clip(self.calc_accel * vfactorBrake, 0, self.params.NIDEC_BRAKE_MAX - 1))         
 # ------------------ brake override end
 
           pcm_override = True
