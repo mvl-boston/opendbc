3736933bd43982fb1491e12db1b5e5e1dfd3faa8
mvl-boston <manuel@alumni.virginia.edu>
2025-05-26 01:26:19 -0400

    remove forwarding from 4 to 5

--- opendbc/safety/safety.h
+++ opendbc/safety/safety.h
@@ -249,8 +249,6 @@ static int get_fwd_bus(int bus_num) {
     destination_bus = 2;
   } else if (bus_num == 2) {
     destination_bus = 0;
-  } else if (bus_num == 4) {
-    destination_bus = 5;
   } else if (bus_num == 5) {
     destination_bus = 4;
   } else {
