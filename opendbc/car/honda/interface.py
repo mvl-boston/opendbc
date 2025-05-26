217ed239a20acaddc7e15f321ab89427b5c41e38
mvl-boston <manuel@alumni.virginia.edu>
2025-05-26 04:38:14 -0400

    adding nidec hybrid definition

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -223,6 +223,10 @@ class CarInterface(CarInterfaceBase):
     if candidate in HONDA_BOSCH_RADARLESS:
       ret.safetyConfigs[-1].safetyParam |= HondaSafetyFlags.RADARLESS.value
 
+    if candidate in HONDA_NIDEC_HYBRID:
+      ret.safetyConfigs[-1].safetyParam |= HondaSafetyFlags.NIDEC_HYBRID.value
+
+
     # min speed to enable ACC. if car can do stop and go, then set enabling speed
     # to a negative value, so it won't matter. Otherwise, add 0.5 mph margin to not
     # conflict with PCM acc
