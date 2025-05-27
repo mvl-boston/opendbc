e6b6f708acb05f3118986d9530cb0f76dd0508dc
mvl-boston <manuel@alumni.virginia.edu>
2025-05-27 00:46:31 -0400

    update steer tuning

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -178,10 +178,9 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.115], [0.052]]
    #   ret.longitudinalActuatorDelay = 1.0 # s
 
-    elif candidate == CAR.ACURA_RLX_HYBRID: # copying MDX
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 239], [0, 239]]
-      ret.lateralTuning.pid.kf = 0.000035
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.115], [0.052]]
+    elif candidate == CAR.ACURA_RLX_HYBRID: # copying Matt
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 32767], [0, 32676]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.1]]
 
     elif candidate == CAR.ACURA_RDX:
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 1000], [0, 1000]]  # TODO: determine if there is a dead zone at the top end
