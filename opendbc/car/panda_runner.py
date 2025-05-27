fb4ac85c79e71a8e7e22c86770edb1218aab8324
mvl-boston <manuel@alumni.virginia.edu>
2025-05-26 22:59:35 -0400

    force all safety output

--- opendbc/car/panda_runner.py
+++ opendbc/car/panda_runner.py
@@ -12,14 +12,17 @@ class PandaRunner(AbstractContextManager):
     self.p.reset()
 
     # setup + fingerprinting
-    self.p.set_safety_mode(CarParams.SafetyModel.elm327, 1)
+#    self.p.set_safety_mode(CarParams.SafetyModel.elm327, 1)
+    self.p.set_safety_mode(CarParams.SafetyModel.SAFETY_ALLOUTPUT, 1)
     self.CI = get_car(self._can_recv, self.p.can_send_many, self.p.set_obd, True)
     assert self.CI.CP.carFingerprint.lower() != "mock", "Unable to identify car. Check connections and ensure car is supported."
 
     safety_model = self.CI.CP.safetyConfigs[0].safetyModel
-    self.p.set_safety_mode(CarParams.SafetyModel.elm327, 1)
+#    self.p.set_safety_mode(CarParams.SafetyModel.elm327, 1)
+    self.p.set_safety_mode(CarParams.SafetyModel.SAFETY_ALLOUTPUT, 1)
     self.CI.init(self.CI.CP, self._can_recv, self.p.can_send_many)
-    self.p.set_safety_mode(safety_model, self.CI.CP.safetyConfigs[0].safetyParam)
+#    self.p.set_safety_mode(safety_model, self.CI.CP.safetyConfigs[0].safetyParam)
+    self.p.set_safety_mode(CarParams.SafetyModel.SAFETY_ALLOUTPUT, 1)
 
     return self
 
