121222c79e695280a8d68d1692de887aa2a27e3d
mvl-boston <manuel@alumni.virginia.edu>
2025-06-01 07:00:42 -0400

    Add files via upload

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -6,7 +6,6 @@ from opendbc.car.disable_ecu import disable_ecu
 from opendbc.car.honda.hondacan import CanBus
 from opendbc.car.honda.values import CarControllerParams, HondaFlags, CAR, HONDA_BOSCH, HONDA_BOSCH_CANFD, \
                                                  HONDA_NIDEC_ALT_SCM_MESSAGES, HONDA_BOSCH_RADARLESS, HondaSafetyFlags, \
-                                                 GasOnlyTuning
 from opendbc.car.honda.carcontroller import CarController
 from opendbc.car.honda.carstate import CarState
 from opendbc.car.honda.radar_interface import RadarInterface
