df6cf03bb5451ce59e1b56cf751533010cfed455
mvl-boston <manuel@alumni.virginia.edu>
2025-06-15 00:44:38 -0400

    Disable SNG for manuals

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -226,7 +226,10 @@ class CarInterface(CarInterfaceBase):
     # min speed to enable ACC. if car can do stop and go, then set enabling speed
     # to a negative value, so it won't matter. Otherwise, add 0.5 mph margin to not
     # conflict with PCM acc
-    ret.autoResumeSng = candidate in (HONDA_BOSCH | {CAR.HONDA_CIVIC})
+    if ret.transmissionType == TransmissionType.manual:
+      then ret.autoResumeSng = False # manuals disable cruise under 25 mph
+    else:
+      ret.autoResumeSng = candidate in (HONDA_BOSCH | {CAR.HONDA_CIVIC})
     ret.minEnableSpeed = -1. if ret.autoResumeSng else 25.51 * CV.MPH_TO_MS
 
     ret.steerActuatorDelay = 0.1
