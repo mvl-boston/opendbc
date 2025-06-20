7db123d8e2c19860a58d8247428d87b65a26839d
mvl-boston <manuel@alumni.virginia.edu>
2025-06-20 07:57:46 -0400

    revert to 3810 fault stuff

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -185,9 +185,9 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.ACURA_RDX_3G_MMR:
       if not (ret.openpilotLongitudinalControl):
         ret.minSteerSpeed = 70.0 * CV.KPH_TO_MS # min is 70kph to activate but 60kph to deactivate.  Used 70kph to clarify for warning message
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2000], [0, 2000]]
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 3810], [0, 3810]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.06]]
-      CarControllerParams.BOSCH_GAS_LOOKUP_V = [0, 2000]
+      CarControllerParams.BOSCH_GAS_LOOKUP_V = [0, 2250]
 
     elif candidate in (CAR.HONDA_ODYSSEY, CAR.HONDA_ODYSSEY_CHN):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.28], [0.08]]
