e90221ccb8c4614c6685b555557817d37ec5a624
mvl-boston <manuel@alumni.virginia.edu>
2025-06-21 21:21:15 -0400

    try steer control change

--- opendbc/car/honda/hondacan.py
+++ opendbc/car/honda/hondacan.py
@@ -122,9 +122,8 @@ def create_steering_control(packer, CAN, apply_torque, lkas_active):
   values = {
     "STEER_TORQUE": apply_torque if lkas_active else 0,
     "STEER_TORQUE_REQUEST": lkas_active,
+    "SET_ME_X00_2": not lkas_active,
   }
-  if CP.carFingerprint in HONDA_BOSCH_ALT_RADAR:
-    values['SET_ME_X00_2'] = not lkas_active,
   return packer.make_can_msg("STEERING_CONTROL", CAN.lkas, values)
 
 
