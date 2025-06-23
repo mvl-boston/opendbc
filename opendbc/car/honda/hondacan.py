feb11b02a6cdacb676dfc05604791cff82039551
mvl-boston <manuel@alumni.virginia.edu>
2025-06-23 01:08:00 -0400

    add fault recovery

--- opendbc/car/honda/hondacan.py
+++ opendbc/car/honda/hondacan.py
@@ -118,11 +118,11 @@ def create_acc_commands(packer, CAN, enabled, active, accel, gas, stopping_count
   return commands
 
 
-def create_steering_control(packer, CAN, apply_torque, lkas_active):
+def create_steering_control(packer, CAN, apply_torque, lkas_active, alt_radar):
   values = {
     "STEER_TORQUE": apply_torque if lkas_active else 0,
     "STEER_TORQUE_REQUEST": lkas_active,
-    "STEER_DOWN_TO_ZERO": lkas_active,
+    "LKAS_FAULT_RECOVERY": alt_radar and lkas_active,
   }
   return packer.make_can_msg("STEERING_CONTROL", CAN.lkas, values)
 
