73e958d1076f9ceb1d0c4a78c3ce628924568c7d
mvl-boston <manuel@alumni.virginia.edu>
2025-06-23 10:37:51 -0400

    update steeringpressed

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -105,6 +105,7 @@ class CarState(CarStateBase):
     self.cruise_setting = 0
     self.v_cruise_pcm_prev = 0
     self.lkas_blocked = False
+    self.steeringTorque_last = 0
 
     # When available we use cp.vl["CAR_SPEED"]["ROUGH_CAR_SPEED_2"] to populate vEgoCluster
     # However, on cars without a digital speedometer this is not always present (HRV, FIT, CRV 2016, ILX and RDX)
