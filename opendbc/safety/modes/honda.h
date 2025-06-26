4bd7b08f364767ebb519d0fabb6966cf75e27b4a
mvl-boston <manuel@alumni.virginia.edu>
2025-06-26 01:06:46 -0400

    bus 0/1/2 ok for steer

--- opendbc/safety/modes/honda.h
+++ opendbc/safety/modes/honda.h
@@ -323,12 +323,16 @@ static safety_config honda_nidec_init(uint16_t param) {
 
 static safety_config honda_bosch_init(uint16_t param) {
   // HONDA_BOSCH_TX_MSGS is used by Bosch and Bosch CAN FD
-  static CanMsg HONDA_BOSCH_TX_MSGS[] = {{0xE4, 1, 5, .check_relay = true}, {0xE5, 1, 8, .check_relay = true},
+  static CanMsg HONDA_BOSCH_TX_MSGS[] = {{0xE4, 0, 5, .check_relay = true}, {0xE5, 0, 8, .check_relay = true},
+                                         {0xE4, 1, 5, .check_relay = true}, {0xE5, 1, 8, .check_relay = true},
+                                         {0xE4, 2, 5, .check_relay = true}, {0xE5, 2, 8, .check_relay = true},
                                          // Send buttons on powertrain bus: 0 for Bosch CAN FD, 1 for CAN
                                          {0x296, 0, 4, .check_relay = false}, {0x296, 1, 4, .check_relay = false},
                                          {0x33D, 0, 5, .check_relay = true}, {0x33DA, 0, 5, .check_relay = true}, {0x33DB, 0, 8, .check_relay = true}};  // Bosch
 
   static CanMsg HONDA_BOSCH_LONG_TX_MSGS[] = {{0xE4, 0, 5, .check_relay = true}, {0x1DF, 0, 8, .check_relay = true}, {0x1EF, 0, 8, .check_relay = false},
+                                              {0xE4, 1, 5, .check_relay = true}, {0x1DF, 1, 8, .check_relay = true}, {0x1EF, 1, 8, .check_relay = false},
+                                              {0xE4, 2, 5, .check_relay = true}, {0x1DF, 2, 8, .check_relay = true}, {0x1EF, 2, 8, .check_relay = false},
                                               {0x1FA, 1, 8, .check_relay = false}, {0x30C, 1, 8, .check_relay = false}, {0x33D, 1, 5, .check_relay = true},
                                               {0x33DA, 1, 5, .check_relay = true}, {0x33DB, 1, 8, .check_relay = true}, {0x39F, 1, 8, .check_relay = false},
                                               {0x18DAB0F1, 1, 8, .check_relay = false}};  // Bosch w/ gas and brakes
@@ -419,6 +423,16 @@ static bool honda_bosch_fwd_hook(int bus_num, int addr) {
     bool is_lkas_msg = (addr == 0xE4) || (addr == 0xE5) || (addr == 0x33D) || (addr == 0x33DA) || (addr == 0x33DB);
     bool is_acc_msg = ((addr == 0x1C8) || (addr == 0x30C)) && honda_bosch_radarless && honda_bosch_long;
     block_msg = is_lkas_msg || is_acc_msg;
+  }
+
+  if (bus_num == 0)  {
+    bool is_lkas_msg = (addr == 0xE4) || (addr == 0xE5) || (addr == 0x33D) || (addr == 0x33DA) || (addr == 0x33DB);
+    block_msg = is_lkas_msg;
+  }
+
+  if (bus_num == 1)  {
+    bool is_lkas_msg = (addr == 0xE4) || (addr == 0xE5) || (addr == 0x33D) || (addr == 0x33DA) || (addr == 0x33DB);
+    block_msg = is_lkas_msg;
   }
 
   return block_msg;
