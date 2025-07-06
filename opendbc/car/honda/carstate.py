18c00bc68defa841da6106bb9e76680eccc3b649
mvl-boston <manuel@alumni.virginia.edu>
2025-07-06 14:59:09 -0400

    above radarless

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -320,7 +320,7 @@ class CarState(CarStateBase):
 
     cam_messages = []
 
-    if  CP.carFingerprint in SERIAL_STEERING:
+    if  CP.carFingerprint in (SERIAL_STEERING, CAR.ACURA_RLX_HYBRID):
       pt_messages += [
         ("STEERING_CONTROL", 0), # initially slow, prevent timing errors
       ]
