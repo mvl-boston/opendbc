c514ca706482410e2db296fa4261fa4382364d4c
mvl-boston <manuel@alumni.virginia.edu>
2025-07-19 11:55:29 -0400

    try lkashud2

--- opendbc/car/honda/hondacan.py
+++ opendbc/car/honda/hondacan.py
@@ -136,7 +136,7 @@ def create_bosch_supplemental_1(packer, CAN):
   return packer.make_can_msg("BOSCH_SUPPLEMENTAL_1", CAN.lkas, values)
 
 
-def create_ui_commands(packer, CAN, CP, enabled, pcm_speed, hud, is_metric, acc_hud, lkas_hud, steer_restricted):
+def create_ui_commands(packer, CAN, CP, enabled, pcm_speed, hud, is_metric, acc_hud, lkas_hud, lkas_hud_2, steer_restricted):
   commands = []
   radar_disabled = CP.carFingerprint in (HONDA_BOSCH - HONDA_BOSCH_RADARLESS) and CP.openpilotLongitudinalControl
 
@@ -179,6 +179,15 @@ def create_ui_commands(packer, CAN, CP, enabled, pcm_speed, hud, is_metric, acc_
   if CP.carFingerprint in (HONDA_BOSCH_RADARLESS | HONDA_BOSCH_CANFD):
     lkas_hud_values['LANE_LINES'] = 3
     lkas_hud_values['DASHED_LANES'] = hud.lanes_visible
+    
+    lkas_hud_2_values = {
+      'SET_ME_X01': 1
+      'COUNTER_2': lkas_hud_2['COUNTER_2']
+      'LKAS_BOH_1': (32 + lkas_hud_values['SOLID_LANES'] * 2) if enabled else 0,
+      'LEFT_LANE': lkas_hud_values['SOLID_LANES'] * 3
+      'RIGHT_LANE': lkas_hud_values['SOLID_LANES'] * 3
+    }
+    commands.append(packer.make_can_msg('LKAS_HUD_2', CAN.lkas, lkas_hud_values))
 
     # car likely needs to see LKAS_PROBLEM fall within a specific time frame, so forward from camera
     # TODO: needed for Bosch CAN FD?
