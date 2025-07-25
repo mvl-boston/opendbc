deb33f872fef53681ce7132723787fba1bd81ae1
mvl-boston <manuel@alumni.virginia.edu>
2025-07-24 23:56:46 -0400

    Add doors back to RLX

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -336,7 +336,7 @@ class CAR(Platforms):
     [], # 2017 RLX Hybrid, don't add to cardocs since custom panda
     CarSpecs(mass=4359 * CV.LB_TO_KG, wheelbase=2.85, centerToFrontRatio=0.39, steerRatio=13.9, tireStiffnessFactor=0.8467),  #spec, stiff/ctf from Accord
     radar_dbc_dict('acura_rlx'),
-    flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES | HondaFlags.NIDEC_HYBRID,
+    flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES | HondaFlags.NIDEC_HYBRID | HondaFlags.HAS_ALL_DOOR_STATES,
   )
   HONDA_PILOT = HondaNidecPlatformConfig(
     [
