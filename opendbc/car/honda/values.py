b9a363c0f8b8b326abcc4a3867c861330509bf3b
mvl-boston <manuel@alumni.virginia.edu>
2025-07-28 18:18:30 -0400

    fix overlapping values

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -69,10 +69,10 @@ class HondaFlags(IntFlag):
   NIDEC = 16
   NIDEC_ALT_PCM_ACCEL = 32
   NIDEC_ALT_SCM_MESSAGES = 64
-  NIDEC_HYBRID = 2048
+  NIDEC_HYBRID = 4096
 
   BOSCH_CANFD = 128
-  BOSCH_ALT_RADAR = 256
+  BOSCH_ALT_RADAR = 2048
 
   HAS_ALL_DOOR_STATES = 256  # Some Hondas have all door states, others only driver door
   HAS_EPB = 512
