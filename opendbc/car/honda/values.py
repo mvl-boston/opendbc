71ebf235629c6f19185aace728a379fa59e3b45d
mvl-boston <manuel@alumni.virginia.edu>
2025-08-01 12:00:15 -0400

    formatting

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -248,7 +248,7 @@ class CAR(Platforms):
   )
   HONDA_ACCORD_11G = HondaBoschCANFDPlatformConfig(
     [HondaCarDocs("Honda Accord 2023-25", "All")],
-    CarSpecs(mass=3,477 * CV.LB_TO_KG, wheelbase=2.83, steerRatio=13.9, centerToFrontRatio=0.39, tireStiffnessFactor=0.8467), # Sport (best selling) trim specs
+    CarSpecs(mass=3477 * CV.LB_TO_KG, wheelbase=2.83, steerRatio=13.9, centerToFrontRatio=0.39, tireStiffnessFactor=0.8467), # Sport (best selling) trim specs
   )
   HONDA_CRV_HYBRID_6G = HondaBoschCANFDPlatformConfig(
     [HondaCarDocs("Honda CR-V Hybrid 2023-25", "All")],
