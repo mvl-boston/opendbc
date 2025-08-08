68a5dde65601b3de97bbecfd6ed7f617e5cbb37d
mvl-boston <manuel@alumni.virginia.edu>
2025-08-07 22:28:44 -0400

    add back alt radar

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -333,6 +333,7 @@ HONDA_NIDEC_ALT_SCM_MESSAGES = CAR.with_flags(HondaFlags.NIDEC_ALT_SCM_MESSAGES)
 HONDA_BOSCH = CAR.with_flags(HondaFlags.BOSCH)
 HONDA_BOSCH_RADARLESS = CAR.with_flags(HondaFlags.BOSCH_RADARLESS)
 HONDA_BOSCH_CANFD = CAR.with_flags(HondaFlags.BOSCH_CANFD)
+HONDA_BOSCH_ALT_RADAR = CAR.with_flags(HondaFlags.BOSCH_ALT_RADAR)
 
 
 DBC = CAR.create_dbc_map()
