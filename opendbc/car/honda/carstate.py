269cb3a78946ae5c28d11c048bc2f013a55e3c98
mvl-boston <manuel@alumni.virginia.edu>
2025-08-14 03:19:56 -0400

    remove block prev

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -41,7 +41,6 @@ class CarState(CarStateBase):
     self.dynamic_v_cruise_units = self.CP.carFingerprint in (HONDA_BOSCH_RADARLESS | HONDA_BOSCH_CANFD)
     self.cruise_setting = 0
     self.v_cruise_pcm_prev = 0
-    self.steer_blocked_prev = False
 
     # When available we use cp.vl["CAR_SPEED"]["ROUGH_CAR_SPEED_2"] to populate vEgoCluster
     # However, on cars without a digital speedometer this is not always present (HRV, FIT, CRV 2016, ILX and RDX)
