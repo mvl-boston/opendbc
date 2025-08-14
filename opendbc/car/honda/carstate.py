1911ff4a7f556c6292a03cd87141118121ddd978
mvl-boston <manuel@alumni.virginia.edu>
2025-08-14 03:17:39 -0400

    cleanup steercontrol

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -87,10 +87,16 @@ class CarState(CarStateBase):
     ret.seatbeltUnlatched = bool(cp.vl["SEATBELT_STATUS"]["SEATBELT_DRIVER_LAMP"] or not cp.vl["SEATBELT_STATUS"]["SEATBELT_DRIVER_LATCHED"])
 
     steer_status = self.steer_status_values[cp.vl["STEER_STATUS"]["STEER_STATUS"]]
-    ret.steerFaultPermanent = steer_status not in ("NORMAL", "NO_TORQUE_ALERT_1", "NO_TORQUE_ALERT_2", "LOW_SPEED_LOCKOUT", "TMP_FAULT", "DRIVER_STEERING")
+    ret.steerFaultPermanent = steer_status not in ("NORMAL", "NO_TORQUE_ALERT_1", "NO_TORQUE_ALERT_2", "LOW_SPEED_LOCKOUT", "TMP_FAULT")
     # LOW_SPEED_LOCKOUT is not worth a warning
     # NO_TORQUE_ALERT_2 can be caused by bump or steering nudge from driver
-    ret.steerFaultTemporary = steer_status not in ("NORMAL", "LOW_SPEED_LOCKOUT", "NO_TORQUE_ALERT_2", "DRIVER_STEERING")
+    if self.CP.carFingerprint in HONDA_BOSCH_ALT_RADAR:
+      # Other states cause a lockout no-steer period 
+      ret.steerFaultTemporary = (steer_status != "NORMAL") or (cp.vl["STEER_STATUS"]["STEER_CONTROL_ACTIVE"] != 1)
+    else:
+      # LOW_SPEED_LOCKOUT is not worth a warning
+      # NO_TORQUE_ALERT_2 can be caused by bump or steering nudge from driver
+      ret.steerFaultTemporary = steer_status not in ("NORMAL", "LOW_SPEED_LOCKOUT", "NO_TORQUE_ALERT_2")
 
     if self.CP.carFingerprint in HONDA_BOSCH_RADARLESS:
       ret.accFaulted = bool(cp.vl["CRUISE_FAULT_STATUS"]["CRUISE_FAULT"])
@@ -111,18 +117,6 @@ class CarState(CarStateBase):
     v_weight = float(np.interp(v_wheel, v_weight_bp, v_weight_v))
     ret.vEgoRaw = (1. - v_weight) * cp.vl["ENGINE_DATA"]["XMISSION_SPEED"] * CV.KPH_TO_MS * self.CP.wheelSpeedFactor + v_weight * v_wheel
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
-
-    if self.CP.carFingerprint in HONDA_BOSCH_ALT_RADAR:
-      ret.lowSpeedAlert = False
-      # under 4mph not worth warning, but don't create falling edge if already in warning state
-      if ret.cruiseState.enabled and (cp.vl["STEER_STATUS"]["STEER_CONTROL_ACTIVE"] != 1) and (self.steer_blocked_prev or ret.vEgo > 4.0 * CV.MPH_TO_MS):
-        if ret.vEgo <= self.CP.minSteerSpeed:
-          ret.lowSpeedAlert = True
-        else:
-          ret.steerFaultTemporary = True
-        self.steer_blocked_prev = True
-      else:
-        self.steer_blocked_prev = False
 
     self.dash_speed_seen = self.dash_speed_seen or cp.vl["CAR_SPEED"]["ROUGH_CAR_SPEED_2"] > 1e-3
     if self.dash_speed_seen:
