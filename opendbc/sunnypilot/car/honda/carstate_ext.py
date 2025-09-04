6cc51fd582104f4b49e4f9cf8600f272eda9937c
Micim987 <4733781+Micim987@users.noreply.github.com>
2025-09-04 12:50:21 -0400

    Update carstate_ext.py
    Added support for clarity brake hold detection

--- opendbc/sunnypilot/car/honda/carstate_ext.py
+++ opendbc/sunnypilot/car/honda/carstate_ext.py
@@ -23,3 +23,5 @@ class CarStateExt:
     if self.CP_SP.flags & HondaFlagsSP.CLARITY:
       ret.accFaulted = bool(cp.vl["HYBRID_BRAKE_ERROR"]["BRAKE_ERROR_1"] or cp.vl["HYBRID_BRAKE_ERROR"]["BRAKE_ERROR_2"])
       ret.stockAeb = bool(cp_cam.vl["BRAKE_COMMAND"]["AEB_REQ_1"] and cp_cam.vl["BRAKE_COMMAND"]["COMPUTER_BRAKE_HYBRID"] > 1e-5)
+      ret.brakeHoldActive = bool(cp.vl["BRAKE_HOLD"]["BRAKE_HOLD_ACTIVE"] == 1)
+      
