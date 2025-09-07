24b1dca63ce880a55adff8ec1209cedb8ec65c0d
mvl-boston <manuel@alumni.virginia.edu>
2025-09-07 02:18:28 -0400

    more cleanup

--- opendbc/safety/safety.h
+++ opendbc/safety/safety.h
@@ -247,8 +247,6 @@ static int get_fwd_bus(int bus_num) {
     destination_bus = 2;
   } else if (bus_num == 2) {
     destination_bus = 0;
-  } else if (bus_num == 5) {
-    destination_bus = 4;
   } else {
     destination_bus = -1;
   }
