e32d89a8c5710319741e9b8f6301b4607f803d22
mvl-boston <manuel@alumni.virginia.edu>
2025-09-07 02:37:07 -0400

    typo fix

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -209,7 +209,7 @@ class CarInterface(CarInterfaceBase):
     # min speed to enable ACC. if car can do stop and go, then set enabling speed
     # to a negative value, so it won't matter. Otherwise, add 0.5 mph margin to not
     # conflict with PCM acc
-    ret.autoResumeSng = candidate in (HONDA_BOSCH | {CAR.HONDA_CIVIC, CCAR.ACURA_RLX_HYBRID})
+    ret.autoResumeSng = candidate in (HONDA_BOSCH | {CAR.HONDA_CIVIC, CAR.ACURA_RLX_HYBRID})
     ret.minEnableSpeed = -1. if ret.autoResumeSng else 25.51 * CV.MPH_TO_MS
 
     ret.steerLimitTimer = 0.8
