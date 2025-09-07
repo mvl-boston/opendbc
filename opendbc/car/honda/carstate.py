a08d8a077c8ceb1806f33a6fa9d6f55004797df6
mvl-boston <manuel@alumni.virginia.edu>
2025-09-07 01:51:26 -0400

    cleanup

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -207,7 +207,7 @@ class CarState(CarStateBase):
       self.acc_hud = cp_cam.vl["ACC_HUD"]
       self.stock_brake = cp_cam.vl["BRAKE_COMMAND"]
     if self.CP.carFingerprint in HONDA_BOSCH_RADARLESS:
-      self.lkas_hud = cp_lkas.vl["LKAS_HUD"]
+      self.lkas_hud = cp_cam.vl["LKAS_HUD"]
 
     if self.CP.enableBsm:
       # BSM messages are on B-CAN, requires a panda forwarding B-CAN messages to CAN 0
