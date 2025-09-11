90c6e0af43efcac74ee66afd6d51ab614f3a7bb9
mvl-boston <manuel@alumni.virginia.edu>
2025-09-11 00:13:56 -0400

    Update values.py

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -306,7 +306,7 @@ class CAR(Platforms):
   HONDA_ODYSSEY_SINGAPORE = HondaNidecPlatformConfig(
     [HondaCarDocs("Honda Odyssey (Singapore) 2021")],
     CarSpecs(mass=1798, wheelbase=2.9, steerRatio=16.2, centerToFrontRatio=0.41),
-    radar_dbc_dict('honda_odyssey_singapore_2021_can_generated'),
+    radar_dbc_dict('honda_odyssey_singapore_2021_can_old_generated'),
     flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES,
   )
   ACURA_RDX = HondaNidecPlatformConfig(
