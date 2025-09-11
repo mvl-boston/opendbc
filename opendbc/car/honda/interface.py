3852750dce41ad62ec35d4286e1c2c25b22377ca
mvl-boston <manuel@alumni.virginia.edu>
2025-09-10 23:12:58 -0400

    Add no_carspeed

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -66,6 +66,9 @@ class CarInterface(CarInterfaceBase):
     if 0x184 in fingerprint[CAN.pt]:
       ret.flags |= HondaFlags.HYBRID.value
 
+    if 0x309 not in fingerprint[CAN.pt]:
+      ret.flags |= HondaFlags.NO_CARSPEED.value
+
     if ret.flags & HondaFlags.ALLOW_MANUAL_TRANS and all(msg not in fingerprint[CAN.pt] for msg in (0x191, 0x1A3)):
       # Manual transmission support for allowlisted cars only, to prevent silent fall-through on auto-detection failures
       ret.transmissionType = TransmissionType.manual
