6913853771637dd2682a4f02f1eef9941e155b29
mvl-boston <manuel@alumni.virginia.edu>
2025-09-10 23:46:55 -0400

    Missing parentheses

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -304,7 +304,7 @@ class CAR(Platforms):
     flags=HondaFlags.NIDEC_ALT_PCM_ACCEL | HondaFlags.HAS_ALL_DOOR_STATES,
   )
   HONDA_ODYSSEY_SINGAPORE = HondaNidecPlatformConfig(
-    [HondaCarDocs("Honda Odyssey (Singapore 2021")],
+    [HondaCarDocs("Honda Odyssey (Singapore) 2021")],
     CarSpecs(mass=1798, wheelbase=2.9, steerRatio=16.2, centerToFrontRatio=0.41),
     radar_dbc_dict('honda_odyssey_singapore_2021_can_generated'),
     flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES,
