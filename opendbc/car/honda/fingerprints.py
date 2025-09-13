8525162886e582d9612e8666c9d004f0f487d070
mvl-boston <manuel@alumni.virginia.edu>
2025-09-13 17:45:00 -0400

    add MDX 3G fingerprints

--- opendbc/car/honda/fingerprints.py
+++ opendbc/car/honda/fingerprints.py
@@ -514,6 +514,44 @@ FW_VERSIONS = {
       b'36161-TDK-J530\x00\x00',
     ],
   },
+  CAR.ACURA_MDX_3G: {
+    (Ecu.vsa, 0x18da28f1, None): [
+      b'57114-TRX-H130\x00\x00',
+      b'57114-TYS-A910\x00\x00',
+      b'57114-TZ6-A810\x00\x00',
+    ],
+    (Ecu.fwdRadar, 0x18dab0f1, None): [
+      b'36161-TRX-A820\x00\x00',
+      b'36161-TYS-A020\x00\x00',
+      b'36161-TYT-A220\x00\x00',
+      b'36161-TZ6-A340\x00\x00',
+      b'36161-TZ6-A730\x00\x00',
+    ],
+    (Ecu.shiftByWire, 0x18da0bf1, None): [
+      b'77959-TZ5-A110\x00\x00',
+      b'54008-TZ5-A710\x00\x00',
+      b'54008-TZ5-A910\x00\x00',
+      b'54008-TZ5-A911\x00\x00',
+    ],
+    (Ecu.transmission, 0x18da1ef1, None): [
+      b'28101-5DH-A400\x00\x00',
+      b'28101-5DH-A401\x00\x00',
+      b'28101-5NC-A740\x00\x00',
+      b'28101-5NC-A770\x00\x00',
+      b'28103-5NC-B210\x00\x00',
+    ],
+    (Ecu.srs, 0x18da53f1, None): [
+      b'77959-TRX-A011\x00\x00',
+      b'77959-TZ5-A110\x00\x00',
+      b'77959-TZ5-A220\x00\x00',
+    ],
+     (Ecu.gateway, 0x18daeff1, None): [
+      b'38897-TRX-A220\x00\x00',
+      b'38897-TYR-A010\x00\x00',
+      b'38897-TYR-A011\x00\x00',
+      b'38897-TZ5-A110\x00\x00',
+    ],
+  },
   CAR.ACURA_MDX_4G_MMR: {
     (Ecu.fwdCamera, 0x18dab5f1, None): [
       b'8S102-TYA-A020\x00\x00',
