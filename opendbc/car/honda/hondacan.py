7d30926b1a8b4e34fb3b28d6a2575a52ac136ae4
mvl-boston <manuel@alumni.virginia.edu>
2025-09-13 21:58:48 -0400

    remove NIDEC_HYBRID

--- opendbc/car/honda/hondacan.py
+++ opendbc/car/honda/hondacan.py
@@ -1,8 +1,7 @@
 import numpy as np
 from opendbc.car import CanBusBase
 from opendbc.car.common.conversions import Conversions as CV
-from opendbc.car.honda.values import HondaFlags, HONDA_BOSCH, HONDA_BOSCH_RADARLESS, HONDA_BOSCH_CANFD, CAR, CarControllerParams, \
-                                     HONDA_NIDEC_HYBRID
+from opendbc.car.honda.values import HondaFlags, HONDA_BOSCH, HONDA_BOSCH_RADARLESS, HONDA_BOSCH_CANFD, CAR, CarControllerParams
 
 # CAN bus layout with relay
 # 0 = ACC-CAN - radar side
