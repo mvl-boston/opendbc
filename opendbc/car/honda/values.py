3ea640eeaab2be6dbeb3b48e1d2aaa028c3a76cf
mvl-boston <manuel@alumni.virginia.edu>
2025-09-13 23:06:59 -0400

    Align values with master

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -73,14 +73,14 @@ class HondaFlags(IntFlag):
   NIDEC_ALT_SCM_MESSAGES = 64
   
   BOSCH_CANFD = 128
-  BOSCH_ALT_RADAR = 2048
 
   HAS_ALL_DOOR_STATES = 256  # Some Hondas have all door states, others only driver door
-  HAS_EPB = 512
+  BOSCH_ALT_RADAR = 512
   ALLOW_MANUAL_TRANS = 1024
   HYBRID = 2048
   HYBRID_ALT_BRAKEHOLD = 4096  # Some Nidec Hybrids use a different brakehold
 
+  HAS_EPB = 8192
 
 # Car button codes
 class CruiseButtons:
