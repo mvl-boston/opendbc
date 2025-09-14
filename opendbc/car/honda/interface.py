06a84454d9bc3d178d5f43afb0ab2a2fcfb3fa15
mvl-boston <manuel@alumni.virginia.edu>
2025-09-13 21:03:12 -0400

    Adding Accord 9G

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -256,6 +256,12 @@ class CarInterface(CarInterfaceBase):
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.18]] # TODO: can probably use some tuning
 
+    elif candidate == CAR.HONDA_ACCORD_9G:
+      ret.steerActuatorDelay = 0.3
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 239], [0, 239]]
+      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.,20], [0.,20]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.4,0.3], [0,0]]
+    
     else:
       raise ValueError(f"unsupported car {candidate}")
 
