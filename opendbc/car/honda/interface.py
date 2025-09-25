1db5b9f7f342738942a51cb53c7474fa532d54bf
MVL <manuel@alumni.virginia.edu>
2025-09-24 23:05:28 -0400

    no lowspeed for manuals

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -280,8 +280,11 @@ class CarInterface(CarInterfaceBase):
     # min speed to enable ACC. if car can do stop and go, then set enabling speed
     # to a negative value, so it won't matter. Otherwise, add 0.5 mph margin to not
     # conflict with PCM acc
-    ret.autoResumeSng = candidate in (HONDA_BOSCH | {CAR.HONDA_CIVIC, CAR.ACURA_MDX_3G, CAR.ACURA_MDX_3G_MMR, CAR.ACURA_RLX, 
-                                                     CAR.ACURA_TLX_1G, CAR.HONDA_CLARITY})
+    if (ret.transmissionType == TransmissionType.manual) and (not ret.openpilotLongitudinalControl):
+      ret.autoResumeSng = False
+    else:
+      ret.autoResumeSng = candidate in (HONDA_BOSCH | {CAR.HONDA_CIVIC, CAR.ACURA_MDX_3G, CAR.ACURA_MDX_3G_MMR, CAR.ACURA_RLX, 
+                                                       CAR.ACURA_TLX_1G, CAR.HONDA_CLARITY})
     ret.minEnableSpeed = -1. if ret.autoResumeSng else 25.51 * CV.MPH_TO_MS
 
     ret.steerLimitTimer = 0.8
