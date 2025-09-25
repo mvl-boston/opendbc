2705e87357c25c7681e0100e839f57eee202a96f
MVL <manuel@alumni.virginia.edu>
2025-09-25 13:48:29 -0400

    remove unneded test routes
    (cherry picked from commit 97573e59f8c7be21df986cbf1791373497f00b5e)

--- opendbc/car/tests/routes.py
+++ opendbc/car/tests/routes.py
@@ -26,6 +26,8 @@ non_tested_cars = [
   HONDA.HONDA_ACCORD_9G,
   HONDA.ACURA_TLX_1G,
   HYUNDAI.GENESIS_G90,
+  HONDA.ACURA_RLX, # TODO: get route once tuning done
+  HONDA.HONDA_CLARITY, # TODO: get route once testing is done
   VOLKSWAGEN.VOLKSWAGEN_CRAFTER_MK2,  # need a route from an ACC-equipped Crafter
   SUBARU.SUBARU_FORESTER_HYBRID,
   VOLKSWAGEN.PORSCHE_MACAN_MK1,
