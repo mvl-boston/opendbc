f97730d9c256fdbb39d9c4a359d97ada4209768f
MVL <manuel@alumni.virginia.edu>
2025-09-28 13:44:47 -0400

    import messaging

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import cereal.messaging as messaging
 
 from opendbc.can import CANPacker
 from opendbc.car import ACCELERATION_DUE_TO_GRAVITY, Bus, DT_CTRL, rate_limit, make_tester_present_msg, structs
