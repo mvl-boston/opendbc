9743d9e5a4818ed1a4c783793e6138cd06157601
MVL <manuel@alumni.virginia.edu>
2025-10-09 19:59:27 -0400

    limit LKAS fix to Bosch A

--- opendbc/car/honda/hondacan.py
+++ opendbc/car/honda/hondacan.py
@@ -198,7 +198,8 @@ def create_lkas_hud(packer, bus, CP, hud_control, lat_active, steering_available
 
   # New HUD concept for selected Bosch cars, overwrites some of the above
   # TODO: make global across all Honda if feedback is favorable
-  if CP.carFingerprint in HONDA_BOSCH: # - try all Bosch, didn't work on Nidec
+  # Try all Bosch A, didn't work on Nidec, and caused LKAS error in Bosch B/C
+  if CP.carFingerprint in HONDA_BOSCH and CP.carFingerprint not in (HONDA_BOSCH_RADARLESS | HONDA_BOSCH_CANFD):
     lkas_hud_values['DASHED_LANES'] = steering_available
     lkas_hud_values['SOLID_LANES'] = lat_active
     lkas_hud_values['LKAS_PROBLEM'] = lat_active and reduced_steering
