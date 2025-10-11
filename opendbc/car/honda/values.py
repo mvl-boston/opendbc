e517b1490e8b3daad9b2d830bd90a43687b3d7ca
MVL <manuel@alumni.virginia.edu>
2025-10-10 23:00:31 -0400

    Add MDX Hybrid

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -378,7 +378,10 @@ class CAR(Platforms):
     flags=HondaFlags.HAS_ALL_DOOR_STATES,
   )
   ACURA_MDX_3G_MMR = HondaNidecPlatformConfig(
-    [HondaCarDocs("Acura MDX 2019-20"],
+    [
+      HondaCarDocs("Acura MDX 2019-20"),
+      HondaCarDocs("Acura MDX Hybrid 2019-20"),
+    ],
     CarSpecs(mass=4215 * CV.LB_TO_KG, wheelbase=2.82, steerRatio=16.8, centerToFrontRatio=0.428),  # as spec, learned steerRatio
     radar_dbc_dict('acura_ilx_2016_can_generated'),
     flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES,
