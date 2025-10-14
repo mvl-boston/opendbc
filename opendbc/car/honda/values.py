2c7c8387d45a01ed9ef783584e003c5b01ec1984
MVL <manuel@alumni.virginia.edu>
2025-10-14 02:07:48 -0400

    Adding 21+ TLX

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -178,6 +178,7 @@ class CAR(Platforms):
     [
       HondaCarDocs("Honda Accord 2023-26", "All"),
       HondaCarDocs("Honda Accord Hybrid 2023-26", "All"),
+      HondaCarDocs("Acura TLX 2024-25", "All"),
   ],
     CarSpecs(mass=3477 * CV.LB_TO_KG, wheelbase=2.83, steerRatio=16.0, centerToFrontRatio=0.39),
   )
@@ -280,6 +281,12 @@ class CAR(Platforms):
     CarSpecs(mass=4590 * CV.LB_TO_KG, wheelbase=3.00, steerRatio=19.4, centerToFrontRatio=0.41),
     {Bus.pt: 'acura_rdx_2020_can_generated'},
     flags=HondaFlags.BOSCH_ALT_BRAKE | HondaFlags.BOSCH_ALT_RADAR,
+  )
+  ACURA_TLX_2G = HondaBoschPlatformConfig(
+    [HondaCarDocs("Acura TLX 2021-23", "All")],
+    CarSpecs(mass=3982 * CV.LB_TO_KG, wheelbase=2.87, steerRatio=14.0, centerToFrontRatio=0.43),
+    {Bus.pt: 'honda_civic_hatchback_ex_2017_can_generated'},
+    flags=HondaFlags.BOSCH_ALT_RADAR,
   )
   ACURA_INTEGRA = HondaBoschPlatformConfig(
     [
