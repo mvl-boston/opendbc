33d308ff8d1294f5d3df581f4285f3e66a7cc56a
MVL <manuel@alumni.virginia.edu>
2025-10-19 00:40:32 -0400

    update to prevent CI errors

--- opendbc/car/psa/carcontroller.py
+++ opendbc/car/psa/carcontroller.py
@@ -13,7 +13,7 @@ class CarController(CarControllerBase):
     self.apply_angle_last = 0
     self.status = 2
 
-  def update(self, CC, CS, now_nanos):
+  def update(self, CC, CC_SP, CS, now_nanos):
     can_sends = []
     actuators = CC.actuators
 
