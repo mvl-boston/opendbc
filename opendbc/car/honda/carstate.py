66d6fefaac2ce50bba7c04e4fb4de747dfaa9ae5
MVL <manuel@alumni.virginia.edu>
2025-10-27 22:58:19 -0400

    missing bus.body secdion

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -249,7 +249,7 @@ class CarState(CarStateBase):
       Bus.pt: CANParser(DBC[CP.carFingerprint][Bus.pt], [], CanBus(CP).pt),
       Bus.cam: CANParser(DBC[CP.carFingerprint][Bus.pt], [], CanBus(CP).camera),
     }
-    if CP.enableBsm:
+    if CP.enableBsm or (self.CP.carFingerprint == CAR.ACURA_RLX):
       parsers[Bus.body] = CANParser(DBC[CP.carFingerprint][Bus.body], [], CanBus(CP).radar)
 
     return parsers
