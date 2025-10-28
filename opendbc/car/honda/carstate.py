aed5ada074d33b874ad33edb24ee65cf7d081a10
MVL <manuel@alumni.virginia.edu>
2025-10-27 22:07:14 -0400

    typo fix

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -126,7 +126,7 @@ class CarState(CarStateBase):
     # Log non-critical stock ACC/LKAS faults if Nidec (camera) or longitudinal CANFD alt-brake
     if self.CP.carFingerprint not in HONDA_BOSCH:
       if self.CP.carFingerprint == CAR.ACURA_RLX:
-        ret.carFaultedNonCritical = bool(cp_cam.vl["ACC_HUD"]["ACC_PROBLEM"]) or cp_lkas.vl["LKAS_HUD"]["LKAS_PROBLEM"])
+        ret.carFaultedNonCritical = bool(cp_cam.vl["ACC_HUD"]["ACC_PROBLEM"] or cp_lkas.vl["LKAS_HUD"]["LKAS_PROBLEM"])
       else:
         ret.carFaultedNonCritical = bool(cp_cam.vl["ACC_HUD"]["ACC_PROBLEM"] or cp_cam.vl["LKAS_HUD"]["LKAS_PROBLEM"])
 
