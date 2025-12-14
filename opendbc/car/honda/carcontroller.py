7d7ea45b6f203eff3866bfbcd83cfd45219b4e0b
MVL <manuel@alumni.virginia.edu>
2025-12-13 23:51:12 -0500

    Try wind factor on pedal interceptor

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -264,7 +264,7 @@ class CarController(CarControllerBase, MadsCarController, GasInterceptorCarContr
             if gas_pedal_force <= 0.0: # don't reduce windfactor while braking, allow increases
               self.windfactor = max(self.windfactor, self.windfactor_before_brake)
             else:
-                self.windfactor_before_brake = self.windfactor
+              self.windfactor_before_brake = self.windfactor
           else:
             gas_pedal_force = self.accel
             # self.gasonly_pid.reset()
@@ -288,9 +288,19 @@ class CarController(CarControllerBase, MadsCarController, GasInterceptorCarContr
           self.brake = apply_brake / self.params.NIDEC_BRAKE_MAX
 
           gas_error = actuators.accel - CS.out.aEgo
-          if gas_error != 0.0 and gas > 0.0:
+          if not CS.out.gasPressed:
+            if gas_error != 0.0 and gas > 0.0:
               self.gasfactor = np.clip(self.gasfactor + gas_error / 50 * (gas * 4.8), 0.1, 3.0)
-          can_sends.extend(GasInterceptorCarController.update(self, CC, CS, gas * self.gasfactor, brake, wind_brake, self.packer, self.frame))
+            if gas_error != 0.0 and (not CS.out.brakePressed) and (CS.out.vEgo > 0.0):
+              wind_adjust = 1 + (wind_brake * 4.8) / 1000
+              self.windfactor = np.clip(self.windfactor * (wind_adjust if (gas_error > 0) else 1.0/wind_adjust), 0.1, 3.0)
+            if gas <= 0.0: # don't reduce windfactor while braking, allow increases
+              self.windfactor = max(self.windfactor, self.windfactor_before_brake)
+            else:
+              self.windfactor_before_brake = self.windfactor
+              
+          can_sends.extend(GasInterceptorCarController.update(self, CC, CS, gas * self.gasfactor, brake, wind_brake * self.windfactor,
+                                                              self.packer, self.frame))
 
     # Send dashboard UI commands.
     if self.frame % 10 == 0:
