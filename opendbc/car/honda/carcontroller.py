fcb964f8177d8435bf25464fce69cbfd8e0ff434
MVL <manuel@alumni.virginia.edu>
2025-12-15 18:12:02 -0500

    doublespeed gap

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -202,7 +202,7 @@ class CarController(CarControllerBase):
       pcm_speed_V = [0.0,
                      np.clip(CS.out.vEgo - 2.0, 0.0, 100.0),
                      np.clip(CS.out.vEgo + 2.0, 0.0, 100.0),
-                     np.clip(CS.out.vEgo + 10.0, 0.0, 100.0)]
+                     np.clip(CS.out.vEgo + 20.0, 0.0, 100.0)]
       pcm_speed = float(np.interp(gas - brake, pcm_speed_BP, pcm_speed_V))
 
       gas_error = actuators.accel - CS.out.aEgo
