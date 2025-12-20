64b253cee5de3a5e85560b769f86e36e47cdb585
MVL <manuel@alumni.virginia.edu>
2025-12-19 21:13:53 -0500

    flip to turning off LKAS

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -251,13 +251,13 @@ class CarController(CarControllerBase):
     # The code below was conditional, only for HONDA_BOSCH_RADARLESS.
     # since I am not radarless, temporarily remove condition for testing.
     if self.CP.carFingerprint in HONDA_BOSCH_RADARLESS:
-      if CS.lkas_ready:
+      if not CS.lkas_ready:
         self.lkas_button_send_remaining = 0
 
       # Start a 5-frame button press to toggle LKAS when it's not in ready state.
       # This activates the LKAS camera to output moving lane lines for the HUD.
       if (CC.enabled and
-          not CS.lkas_ready and
+          CS.lkas_ready and
           self.lkas_button_send_remaining == 0 and
           self.frame >= self.last_lkas_button_frame + 100): # Wait 100 frames for HUD to update
         self.lkas_button_send_remaining = 5
