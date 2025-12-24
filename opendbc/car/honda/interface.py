483397daec046ea8782d32f80fd5e963d6f6866c
MVL <manuel@alumni.virginia.edu>
2025-12-24 18:40:57 -0500

    Enable SNG for all pedalInterceptor cars

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -413,6 +413,7 @@ class CarInterface(CarInterfaceBase):
       ret.safetyParam |= HondaSafetyFlagsSP.GAS_INTERCEPTOR
 
     stock_cp.autoResumeSng = stock_cp.autoResumeSng or ret.enableGasInterceptor
+    stock_cp.minEnableSpeed = -1. if ret.enableGasInterceptor else stock_cp.minEnableSpeed
 
     return ret
 
