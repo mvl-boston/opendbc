56ca3b89898420ced3061dbee4959bcf0b441cfb
Rick Lan <ricklan@gmail.com>
2025-12-26 13:43:56 +0800

    Revert "long tunes from 0.9.5"
    This reverts commit f0443158444667809ea46722dde23edf6aeaeebc.

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -164,11 +164,6 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.HONDA_ODYSSEY_TWN:
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.28], [0.08]]
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 32767], [0, 32767]]  # TODO: determine if there is a dead zone at the top end
-      # tunes from 0.9.5
-      ret.longitudinalTuning.kpBP = [0., 5., 35.]
-      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
-      ret.longitudinalTuning.kiBP = [0., 35.]
-      ret.longitudinalTuning.kiV = [0.18, 0.12]
 
     elif candidate == CAR.HONDA_PILOT:
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
