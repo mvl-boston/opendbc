01f62ccf78a4ddfcbab0e31cccfc869b7d5527ad
MVL <manuel@alumni.virginia.edu>
2025-12-31 03:41:43 -0500

    move to efinian steer

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -363,7 +363,7 @@ class CAR(Platforms):
   )
   HONDA_ODYSSEY_TWN = HondaNidecPlatformConfig(
     [],
-    CarSpecs(mass=1865, wheelbase=2.9, steerRatio=17.6, centerToFrontRatio=0.44),
+    CarSpecs(mass=1900, wheelbase=3.0, steerRatio=14.35, centerToFrontRatio=0.41, tireStiffnessFactor=0.82),
     radar_dbc_dict('honda_odyssey_singapore_2021_can_generated'),
     flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES,
   )
