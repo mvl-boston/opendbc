0a4845c19adb5722f8e091af54d23eee310a6e66
MVL <manuel@alumni.virginia.edu>
2026-01-09 12:42:54 -0500

    8600 for mdx

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -206,7 +206,7 @@ class CarInterface(CarInterfaceBase):
       CarControllerParams.NIDEC_ACCEL_MAX = 1.0
 
     elif candidate in (CAR.HONDA_PILOT, CAR.HONDA_PILOT_4G, CAR.HONDA_PASSPORT_4G, CAR.ACURA_MDX_4G_MMR):
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 8600], [0, 8600]]  # TODO: determine if there is a dead zone at the top end
       # ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.38], [0.11]] replace w Marco tune below
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kpV = [[0, 10], [0.05, 0.5]]
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kiV = [[0, 10], [0.0125, 0.125]]
