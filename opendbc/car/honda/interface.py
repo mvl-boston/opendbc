6da848a942bcfbd65ab3951aa95addb79c58d4bb
MVL <manuel@alumni.virginia.edu>
2026-01-21 21:28:03 -0500

    Increase Accord 11G to 6966
    (cherry picked from commit 54800a1e3884125826db0ced7528f9c698220050)

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -133,6 +133,12 @@ class CarInterface(CarInterfaceBase):
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.18]]
 
+    elif candidate == CAR.HONDA_ACCORD_11G:
+      ret.steerActuatorDelay = 0.22
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 6966], [0, 6966]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.18]]
+      CarControllerParams.BOSCH_GAS_LOOKUP_V = [0, 1060]
+
     elif candidate == CAR.ACURA_ILX:
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 3840], [0, 3840]]  # TODO: determine if there is a dead zone at the top end
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.8], [0.24]]
