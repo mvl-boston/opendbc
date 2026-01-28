4172275f43dfbc5f49132db5b7c1ace4bf8b5d64
Shane Smiskol <shane@smiskol.com>
2026-01-27 22:51:45 -0800

    honda: init class variables in __init__

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -50,6 +50,8 @@ class CarState(CarStateBase):
     # When available we use cp.vl["CAR_SPEED"]["ROUGH_CAR_SPEED_2"] to populate vEgoCluster
     # However, on cars without a digital speedometer this is not always present (HRV, FIT, CRV 2016, ILX and RDX)
     self.dash_speed_seen = False
+    self.is_metric = False
+    self.v_cruise_factor = 1.
 
     self.initial_accFault_cleared = False
     self.initial_accFault_cleared_timer = int(10 / DT_CTRL) # 10 seconds after startup for initial faults to clear
