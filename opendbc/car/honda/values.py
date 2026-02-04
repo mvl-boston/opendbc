6ea9cc62916ce708ce6fbacd25d44fd282e0eb28
MVL <manuel@alumni.virginia.edu>
2026-02-03 22:38:59 -0500

    move 2019 MDX to pre-MMR steer

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -419,8 +419,8 @@ class CAR(Platforms):
   ACURA_MDX_3G = HondaNidecPlatformConfig(
     [
       HondaCarDocs("Acura MDX 2014-16", "Advance Package"),
-      HondaCarDocs("Acura MDX 2017-18", "All"),
-      HondaCarDocs("Acura MDX Hybrid 2017-18", "All"),
+      HondaCarDocs("Acura MDX 2017-19", "All"),
+      HondaCarDocs("Acura MDX Hybrid 2017-19", "All"),
     ],
     CarSpecs(mass=4215 * CV.LB_TO_KG, wheelbase=2.82, steerRatio=16.8, centerToFrontRatio=0.428),  # as spec, learned steerRatio
     radar_dbc_dict('acura_mdx_2017_can_ext_generated'),
@@ -428,8 +428,8 @@ class CAR(Platforms):
   )
   ACURA_MDX_3G_MMR = HondaNidecPlatformConfig(
     [
-      HondaCarDocs("Acura MDX 2019-20", "All"),
-      HondaCarDocs("Acura MDX Hybrid 2019-20", "All"),
+      HondaCarDocs("Acura MDX 2020", "All"),
+      HondaCarDocs("Acura MDX Hybrid 2020", "All"),
     ],
     CarSpecs(mass=4215 * CV.LB_TO_KG, wheelbase=2.82, steerRatio=16.8, centerToFrontRatio=0.428),  # as spec, learned steerRatio
     radar_dbc_dict('acura_ilx_2016_can_generated'),
