07507917521d61be94a2a53b0311d9268e009ba5
MVL <manuel@alumni.virginia.edu>
2026-02-05 23:12:04 -0500

    Adding Fit 4G, pulling centerToFront from Nidec Fit
    (cherry picked from commit 978f45aed21211114f5f11cc20264bd0dde0c4b7)

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -307,6 +307,18 @@ class CAR(Platforms):
     [HondaCarDocs("Acura TLX 2025", "All")],
     CarSpecs(mass=3990 * CV.LB_TO_KG, wheelbase=2.87, centerToFrontRatio=0.43, steerRatio=13.7),
   )
+  HONDA_FIT_4G = HondaBoschPlatformConfig(
+    [HondaCarDocs("Honda Fit (Taiwan) 2025", "All")],
+    CarSpecs(mass=1229, wheelbase=2.53, steerRatio=15.2, centerToFrontRatio=0.39),
+    {Bus.pt: 'honda_bosch_radarless_generated'},
+    flags=HondaFlags.BOSCH_RADARLESS,
+  )
+  ACURA_INTEGRA = HondaBoschPlatformConfig(
+    [HondaCarDocs("Acura Integra 2023-25", "All")],
+    CarSpecs(mass=3338.8 * CV.LB_TO_KG, wheelbase=2.5, centerToFrontRatio=0.5, steerRatio=15.5,),
+    {Bus.pt: 'honda_bosch_radarless_generated'},
+    flags=HondaFlags.BOSCH_RADARLESS | HondaFlags.ALLOW_MANUAL_TRANS,
+  )
   ACURA_ADX = HondaBoschPlatformConfig(
     [HondaCarDocs("Acura ADX 2025", "All")],
     CarSpecs(mass=3578 * CV.LB_TO_KG, wheelbase=2.65, steerRatio=14.35, centerToFrontRatio=0.43),
