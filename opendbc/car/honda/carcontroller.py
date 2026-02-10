cb2011f49b5ac23383c6cf57bd3b2177c4ef2b6c
MVL <manuel@alumni.virginia.edu>
2026-02-10 07:07:30 -0500

    Add Fit 4G to higher speed cutoff

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -148,7 +148,7 @@ class CarController(CarControllerBase):
         targetspeed = 2.2352 # target 5mph within 3 seconds
         if self.CP.carFingerprint in (CAR.HONDA_NBOX_2G):
           targetspeed = 5 + 1
-        if self.CP.carFingerprint in (CAR.HONDA_CITY_7G):
+        if self.CP.carFingerprint in (CAR.HONDA_CITY_7G, CAR.HONDA_FIT_4G):
           targetspeed = 6.4 + 1
         if targetspeed < self.CP.minEnableSpeed + 1:
           targetspeed = self.CP.minEnableSpeed + 1
