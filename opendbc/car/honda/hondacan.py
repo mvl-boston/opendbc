243bf1be4e62c3ff13fe2d32bbde140bb78f80d9
MVL <manuel@alumni.virginia.edu>
2026-02-26 04:02:00 -0500

    try new MVL instrument cluster

--- opendbc/car/honda/hondacan.py
+++ opendbc/car/honda/hondacan.py
@@ -183,11 +183,12 @@ def create_lkas_hud(packer, bus, CP, hud_control, lat_active, steering_available
     'LKAS_READY': 1,
     'LKAS_STATE_CHANGE': 1,
     'STEERING_REQUIRED': alert_steer_required,
-    'SOLID_LANES': hud_control.lanesVisible and not steer_maxed,
-    'DASHED_LANES': lat_active,
+    'SOLID_LANES': lat_active,
     'BEEP': 0,
   }
 
+  lkas_hud_values['DASHED_LANES'] = lkas_hud_values['SOLID_LANES'] and hud_control.lanesVisible and steering_available and not steer_maxed
+  
   if CP.carFingerprint in (HONDA_BOSCH_RADARLESS | HONDA_BOSCH_CANFD):
     lkas_hud_values['LANE_LINES'] = 3
     lkas_hud_values['DASHED_LANES'] = hud_control.lanesVisible
