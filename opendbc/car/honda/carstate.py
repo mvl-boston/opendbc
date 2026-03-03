f0aa505482bd0958735919db49f7c955cd42a318
MVL <manuel@alumni.virginia.edu>
2026-03-02 21:54:37 -0500

    temp gearshifter override for Honda Fit

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -169,7 +169,10 @@ class CarState(CarStateBase):
       ret.blockPcmEnable = ret.brakeHoldActive # Nidec Hybrids fault if resuming cruise from brake hold
 
     if self.CP.transmissionType == TransmissionType.manual:
-      ret.gearShifter = GearShifter.reverse if bool(cp.vl["SCM_FEEDBACK"]["REVERSE_LIGHT"]) else GearShifter.drive
+      if self.CP.carfingerprint == CAR.HONDA_FIT:
+        ret.gearShifter = GearShifter.drive # temp override until message found for Nidec
+      else:
+        ret.gearShifter = GearShifter.reverse if bool(cp.vl["SCM_FEEDBACK"]["REVERSE_LIGHT"]) else GearShifter.drive
     else:
       gear_position = self.shifter_values.get(cp.vl[self.gearbox_msg]["GEAR_SHIFTER"], None)
       ret.gearShifter = self.parse_gear_shifter(gear_position)
