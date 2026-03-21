b79a93f58e98fa98ac18ad8993dac1caad8dac47
MVL <manuel@alumni.virginia.edu>
2026-03-20 22:05:30 -0400

    add prior steermax for Pilot 4G MMR

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -262,6 +262,11 @@ class CarInterface(CarInterfaceBase):
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
+    elif candidate == CAR.HONDA_PILOT_4G_MMR:
+      ret.steerActuatorDelay = 0.15
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4200], [0, 4200]]
+      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
+
     else:
       ret.steerActuatorDelay = 0.15
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 3840], [0, 3840]]
