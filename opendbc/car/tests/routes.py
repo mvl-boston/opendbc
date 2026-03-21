5c8e3c8986314a5196e1795af00e98bf10c50579
MVL <manuel@alumni.virginia.edu>
2026-03-20 21:12:14 -0400

    temp 26 Pilot route exception
    (cherry picked from commit 405833d9523399d706d2ae8e6c8ac752a2b0af9a)

--- opendbc/car/tests/routes.py
+++ opendbc/car/tests/routes.py
@@ -32,6 +32,10 @@ non_tested_cars = [
   SUBARU.SUBARU_FORESTER_HYBRID,
   VOLKSWAGEN.PORSCHE_MACAN_MK1,
   HONDA.ACURA_TLX_2G,
+  HONDA.HONDA_CITY_7G,
+  HONDA.ACURA_RDX_3G_MMR,
+  HONDA.ACURA_INTEGRA,
+  HONDA.PILOT_4G_MMR,
 
   # These had their DSUs unplugged, need new routes
   # TOYOTA.LEXUS_ES  # hybrid
