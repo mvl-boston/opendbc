f5fea227257562d3dfa5050cd293da76dd39a749
MVL <manuel@alumni.virginia.edu>
2026-03-22 10:19:17 -0400

    stop openpilot pid controller

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -100,8 +100,9 @@ class CarInterface(CarInterfaceBase):
         ret.stopAccel = CarControllerParams.BOSCH_ACCEL_MIN  # stock uses -4.0 m/s^2 once stopped but limited by safety model
     else:
       # default longitudinal tuning for all Nidec hondas
-      ret.longitudinalTuning.kiBP = [0., 5., 35.]
-      ret.longitudinalTuning.kiV = [1.2, 0.8, 0.5]
+      # ret.longitudinalTuning.kiBP = [0., 5., 35.]
+      # ret.longitudinalTuning.kiV = [1.2, 0.8, 0.5]
+      pass # moved to opendbc controller
 
     if True: # candidate == CAR.HONDA_CITY_7G: - 5mph stopping all cars
       ret.vEgoStopping = 1.0
