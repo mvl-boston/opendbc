1e8f65ea63c4204308f9336a49fbeef0aff80fa1
MVL <manuel@alumni.virginia.edu>
2026-03-22 22:43:59 -0400

    complete nidec pid

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -159,8 +159,8 @@ class CarController(CarControllerBase):
         self.nidec_pid.reset()
       self.prior_accel = actuators.accel
       stopaccel = min(actuators.accel, self.ai_stopping) * (morebrakefactor if actuators.accel < 0.0 else 1.0)
+      stopaccel += self.nidec_pid.update(error = stopaccel - CS.out.aEgo, speed = CS.out.vEgo)
       accel = stopaccel
-      self.nidec_pid.update(error = accel - CS.out.aEgo, speed = CS.out.vEgo)
       gas, brake = compute_gas_brake(stopaccel + hill_brake, CS.out.vEgo, self.CP.carFingerprint)
     else:
       accel = 0.0
