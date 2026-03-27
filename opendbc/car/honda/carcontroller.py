2751b57c06a5bcd59c9d18e88428a96ff17e0ef1
MVL <manuel@alumni.virginia.edu>
2026-03-27 19:46:16 -0400

    Try RDX turbolag tuning

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -241,8 +241,14 @@ class CarController(CarControllerBase, MadsCarController, GasInterceptorCarContr
           # live-learn gas pedal adjustments when openpilot is controlling gas
           if (actuators.longControlState == LongCtrlState.pid) and (not CS.out.gasPressed):
             gas_error = self.accel - CS.out.aEgo
+            if (self.CP.carFingerprint == CAR.ACURA_RDX_3G and CS.out.vEgo < 1e-3):
+              self.gasfactor = 3.0 # max due to turbolag
             if gas_error != 0.0 and gas_pedal_force > 0.0:
-              learn_speed = 150 if (self.CP.carFingerprint == CAR.HONDA_INSIGHT) else 50 # Insight gas pedal reacts too slowly
+              if (self.CP.carFingerprint == CAR.HONDA_INSIGHT): # Insight gas pedal reacts too slowly
+                learn_speed = 150
+              elif (self.CP.carFingerprint == CAR.ACURA_RDX_3G): # make up for turbolag faster
+                learn_speed = 20
+              else learn_speed = 50
               self.gasfactor = np.clip(self.gasfactor + gas_error / learn_speed * gas_pedal_force, 0.1, 3.0)
             if gas_error != 0.0 and (not CS.out.brakePressed) and (CS.out.vEgo > 0.0):
               wind_adjust = 1 + wind_brake_ms2 / 1000
