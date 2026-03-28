f754911e52f7d1a4446c1842a6489ef007bdb291
Jason Young <46612682+jyoung8607@users.noreply.github.com>
2026-03-27 19:48:27 -0700

    Honda: Democratize the manual transmission (#3245)
    * REVERSE_LIGHT for nidec_scm_group_a cars
    
    * use car_state_scm_message as the source of REVERSE_LIGHT
    
    * retire the ALLOW_MANUAL_TRANS flag
    
    (cherry picked from commit 2081e774a5818cee6bee06062ffef145b7465578)
    (cherry picked from commit 2deef905d300604b200541a0fd1e0d79c7d1fa8b)

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -162,8 +162,7 @@ class CarState(CarStateBase):
       ret.blockPcmEnable = ret.brakeHoldActive # Nidec Hybrids fault if resuming cruise from brake hold
 
     if self.CP.transmissionType == TransmissionType.manual:
-      reverse_light_msg = cp.vl["SCM_FEEDBACK"] if self.CP.carFingerprint in HONDA_BOSCH else cp.vl["SCM_BUTTONS"]
-      ret.gearShifter = GearShifter.reverse if bool(reverse_light_msg["REVERSE_LIGHT"]) else GearShifter.drive
+      ret.gearShifter = GearShifter.reverse if bool(cp.vl[self.car_state_scm_msg]["REVERSE_LIGHT"]) else GearShifter.drive
     else:
       gear_position = self.shifter_values.get(cp.vl[self.gearbox_msg]["GEAR_SHIFTER"], None)
       ret.gearShifter = self.parse_gear_shifter(gear_position)
