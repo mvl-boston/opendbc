046e29d39c89daaec25cc756f62b9e6e566d89ef
MVL <manuel@alumni.virginia.edu>
2026-03-28 05:26:02 -0400

    even higher steermax

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -264,7 +264,7 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate in (CAR.ACURA_MDX_4G):
       ret.steerActuatorDelay = 0.15
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4450], [0, 4450]]
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 9150], [0, 9150]]
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     elif candidate == CAR.HONDA_PILOT_4G_MMR:
