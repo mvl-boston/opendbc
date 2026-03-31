368553703dac54d3cc3ccddb03ff2240617990d1
MVL <manuel@alumni.virginia.edu>
2026-03-31 00:31:19 -0400

    update RDX launch gas

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -242,7 +242,7 @@ class CarController(CarControllerBase, MadsCarController, GasInterceptorCarContr
           if (actuators.longControlState == LongCtrlState.pid) and (not CS.out.gasPressed):
             gas_error = self.accel - CS.out.aEgo
             if (self.CP.carFingerprint == CAR.ACURA_RDX_3G and CS.out.vEgo < 1e-3):
-              self.gasfactor = 3.0 # max due to turbolag
+              self.gasfactor = 1.6 # higher for launch from standstill due to turbolag
             if gas_error != 0.0 and gas_pedal_force > 0.0:
               if self.CP.carFingerprint in (CAR.HONDA_INSIGHT, CAR.ACURA_RDX_3G): # Insight gas pedal reacts too slowly
                 learn_speed = 150
