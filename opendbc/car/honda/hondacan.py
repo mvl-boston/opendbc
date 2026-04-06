3bd47f1968a6a6b3ed4c03c4c4f122d3f566786d
mvl <manuel@alumni.virginia.edu>
2026-04-05 22:58:59 -0400

    Revert "Try LKAS_HUD timing for 4G passport"
    This reverts commit 35e48a9f9d02c4dca32a857be47408b83a05c56d.

--- opendbc/car/honda/hondacan.py
+++ opendbc/car/honda/hondacan.py
@@ -191,7 +191,7 @@ def create_lkas_hud(packer, bus, CP, hud_control, lat_active, steering_available
 
     # car likely needs to see LKAS_PROBLEM fall within a specific time frame, so forward from camera
     # TODO: needed for Bosch CAN FD?
-    if (CP.carFingerprint in HONDA_BOSCH_RADARLESS) or (CP.carFingerprint == CAR.HONDA_PASSPORT_4G):
+    if CP.carFingerprint in HONDA_BOSCH_RADARLESS:
       lkas_hud_values['LKAS_PROBLEM'] = lkas_hud['LKAS_PROBLEM']
 
   if not (CP.flags & HondaFlags.BOSCH_EXT_HUD):
