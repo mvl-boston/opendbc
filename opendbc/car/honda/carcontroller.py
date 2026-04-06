38bcd1c99948e1baf7dea243378755fd91e077cf
MVL <manuel@alumni.virginia.edu>
2026-04-06 00:23:25 -0400

    add average_factor limit

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -244,7 +244,7 @@ class CarController(CarControllerBase):
       if more_new_accel_needed:
         self.average_factor /= 1.0001
       else:
-        self.average_factor *= 1.0001
+        self.average_factor = min(1.0, self.average_factor * 1.0001)
 
     if not self.CP.openpilotLongitudinalControl:
       if self.frame % 2 == 0 and self.CP.carFingerprint not in HONDA_BOSCH_RADARLESS | HONDA_BOSCH_CANFD:
