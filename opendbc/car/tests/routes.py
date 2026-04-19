bc9c0444dda2726bb3cad7bd640b84866d300131
MVL <manuel@alumni.virginia.edu>
2026-04-19 10:45:38 -0400

    Add HONDA_PILOT_4G_MMR to routes list

--- opendbc/car/tests/routes.py
+++ opendbc/car/tests/routes.py
@@ -31,6 +31,7 @@ non_tested_cars = [
   HONDA.ACURA_TLX_2G, # need new route - expired
   HONDA.HONDA_CRV_SA, # need new route - expired
   HONDA.ACURA_TLX_2G_MMR, # need to fix Integra bug causing error
+  HONDA.HONDA_PILOT_4G_MMR, # awaiting driver for testroute
 
   # port extensions
   HYUNDAI.KIA_CEED_PHEV_2022_NON_SCC,
