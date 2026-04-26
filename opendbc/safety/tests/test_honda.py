21c1280e236a8e2563c7f45e45d5801bb03bb0f9
MVL <manuel@alumni.virginia.edu>
2026-04-25 21:45:21 -0400

    try no priming

--- opendbc/safety/tests/test_honda.py
+++ opendbc/safety/tests/test_honda.py
@@ -570,11 +570,8 @@ class TestHondaBoschRadarlessLongNoEngineDataMsgSafety(TestHondaBoschRadarlessLo
     self.safety.set_safety_hooks(CarParams.SafetyModel.hondaBosch,
                                  HondaSafetyFlags.RADARLESS | HondaSafetyFlags.BOSCH_LONG | HondaSafetyFlags.NO_ENGINE_DATA_MSG)
     self.safety.init_tests()
-    cnt_speed_before_prime = self.__class__.cnt_speed
     self._abs_tick = 0
     self._rx(self._abs_sensor_msg(self._abs_tick))
-    # Keep inherited counter-based tests on the same phase as other Honda classes.
-    self.__class__.cnt_speed = cnt_speed_before_prime
 
   def _abs_sensor_msg(self, abs_sensor):
     values = {
