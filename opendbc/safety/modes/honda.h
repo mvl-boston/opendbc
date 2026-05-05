4f08170da7939641b66fb0a32fbb5b86de11c7e1
MVL <manuel@alumni.virginia.edu>
2026-05-05 07:34:56 -0400

    try cluster ECU

--- opendbc/safety/modes/honda.h
+++ opendbc/safety/modes/honda.h
@@ -263,7 +263,7 @@ static bool honda_tx_hook(const CANPacket_t *msg) {
   }
 
   // Only tester present ("\x02\x3E\x80\x00\x00\x00\x00\x00") allowed on diagnostics address
-  if (msg->addr == 0x18DAB0F1U) {
+  if (msg->addr == 0x18DAF160U) {
     tx = true;
     // if ((GET_BYTES(msg, 0, 4) != 0x00803E02U) || (GET_BYTES(msg, 4, 4) != 0x0U)) {
     //   tx = false;
@@ -278,7 +278,7 @@ static safety_config honda_nidec_init(uint16_t param) {
   // 0xE4 is steering on all cars except CRV and RDX, 0x194 for CRV and RDX,
   // 0x1FA is brake control, 0x30C is acc hud, 0x33D is lkas hud
   static CanMsg HONDA_N_TX_MSGS[] = {{0xE4, 0, 5, .check_relay = false}, {0x194, 0, 4, .check_relay = false}, {0x1FA, 0, 8, .check_relay = false},
-                                     {0x30C, 0, 8, .check_relay = false}, {0x33D, 0, 4, .check_relay = false}, {0x18DAB0F1, 0, 8, .check_relay = false}};
+                                     {0x30C, 0, 8, .check_relay = false}, {0x33D, 0, 4, .check_relay = false}, {0x18DAF160, 0, 8, .check_relay = false}};
 
   const uint16_t HONDA_PARAM_NIDEC_ALT = 4;
 
