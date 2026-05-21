a7cca2dcec433b5ebabbf74a5bfada224b0bb6f5
MVL <manuel@alumni.virginia.edu>
2026-05-20 22:45:04 -0400

    tlx_1g

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -428,7 +428,7 @@ class CAR(Platforms):
   ACURA_TLX_1G = HondaNidecPlatformConfig(
     [], # don't add to cardocs since custom steering board
     CarSpecs(mass=3680 * CV.LB_TO_KG, wheelbase=2.78, steerRatio=15.1, centerToFrontRatio=0.40),  # as spec
-    radar_dbc_dict('acura_mdx_2016_can_generated'),
+    radar_dbc_dict('acura_mdx_3g_can_generated'),
     flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES | HondaFlags.HAS_ALL_DOOR_STATES,
   )
   HONDA_CLARITY = HondaNidecPlatformConfig(
