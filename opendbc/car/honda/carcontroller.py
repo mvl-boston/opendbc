795c21dd533fa11edd61e3735109542004401d50
MVL <manuel@alumni.virginia.edu>
2026-05-23 17:14:06 -0400

    init cleanup

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -143,7 +143,7 @@ class CarController(CarControllerBase):
     self.gasfactor = 1.0 if (Params().get("HondaGasFactorParams") is None) else Params().get("HondaGasFactorParams")
     self.gasfactor_before_gasmax = self.gasfactor
     self.windfactor = 1.0 if (Params().get("HondaWindFactorParams") is None) else Params().get("HondaWindFactorParams")
-    self.windfactor_before_maxgas = self.windfactor_before_brake = self.windfactor
+    self.windfactor_before_gasmax = self.windfactor_before_brake = self.windfactor
     self.new_accel = 0.0
 
     self.latFactors = {
