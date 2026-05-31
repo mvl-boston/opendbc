19d366e70d915296094985083aba625a03ee246b
MVL <manuel@alumni.virginia.edu>
2026-05-30 23:26:01 -0400

    allow creep_always to learn both ways

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -249,8 +249,8 @@ class CarController(CarControllerBase):
       gas_error = self.accel - CS.out.aEgo
       if (actuators.longControlState == LongCtrlState.pid) and (not CS.out.stockAeb) and (not CS.out.gasPressed) \
              and (1e-5 <= CS.out.vEgo <= CS.out.cruiseState.speed - 2.):
-        self.creep_factor += np.clip(0.001 * creep_impact * gas_error, 0.0, 3.0)
-        self.creep_always += np.clip(0.0001 * gas_error, -3.5, 0.0)
+        self.creep_factor = np.clip(self.creep_factor + 0.001 * creep_impact * gas_error, 0.0, 3.0)
+        self.creep_always = np.clip(self.creep_always + 0.0001 * gas_error, -3.5, 0.0)
     else:
       self.accel = 0.0
       adjust_accel = self.accel
