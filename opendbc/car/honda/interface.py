46913f99288499c779ee9fe37601073c57efed6d
MVL <manuel@alumni.virginia.edu>
2026-06-05 20:59:21 -0400

    Merge 0.11.1 op base
    Merge 0.11.1 op base

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -283,8 +283,7 @@ class CarInterface(CarInterfaceBase):
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     # These cars use alternate user brake msg (0x1BE)
-    if 0x1BE in fingerprint[CAN.pt] and candidate in (CAR.HONDA_ACCORD, CAR.HONDA_HRV_3G, CAR.ACURA_ADX, CAR.ACURA_MDX_4G,
-                                                      CAR.ACURA_RDX_3G, *HONDA_BOSCH_CANFD):
+    if 0x1BE in fingerprint[CAN.pt]:
       ret.flags |= HondaFlags.BOSCH_ALT_BRAKE.value
 
     if ret.flags & HondaFlags.BOSCH_ALT_BRAKE:
