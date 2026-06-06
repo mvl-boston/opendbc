8d8ff70f7ea8fe4def0db949a605000d34d51dc2
MVL <manuel@alumni.virginia.edu>
2026-06-06 02:10:24 -0400

    remove nonblocking

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -120,7 +120,7 @@ class HondaParamWriter:
         pass
 
       for key, value in pending.items():
-        self._params.put_nonblocking(key, value)
+        self._params.put(key, value)
 
 
 class CarController(CarControllerBase, MadsCarController, GasInterceptorCarController, IntelligentCruiseButtonManagementInterface):
