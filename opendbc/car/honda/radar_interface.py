317d459e542ad7dd5f5fa5def7d0f9f82395610e
MVL <manuel@alumni.virginia.edu>
2026-06-10 01:03:28 -0400

    Add required imports

--- opendbc/car/honda/radar_interface.py
+++ opendbc/car/honda/radar_interface.py
@@ -6,7 +6,7 @@ from opendbc.can import CANParser
 from opendbc.car import Bus, structs
 from opendbc.car.interfaces import RadarInterfaceBase
 from opendbc.car.honda.hondacan import CanBus
-from opendbc.car.honda.values import DBC, CAR
+from opendbc.car.honda.values import DBC, CAR, HONDA_BOSCH, HONDA_BOSCH_CANFD, HONDA_BOSCH_RADARLESS
 
 
 def _create_nidec_can_parser(car_fingerprint):
