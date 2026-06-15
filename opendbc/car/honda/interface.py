530b0e63c27292044e9cac540e30ffb0482eff1a
MVL <manuel@alumni.virginia.edu>
2026-06-15 14:42:17 -0400

    restrict bosch_alt_brake to bosch

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -283,7 +283,7 @@ class CarInterface(CarInterfaceBase):
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     # These cars use alternate user brake msg (0x1BE)
-    if 0x1BE in fingerprint[CAN.pt]:
+    if (candidate in HONDA_BOSCH) and (0x1BE in fingerprint[CAN.pt]):
       ret.flags |= HondaFlags.BOSCH_ALT_BRAKE.value
 
     if 0x190 in fingerprint[CAN.pt] and candidate in (CAR.ACURA_MDX_3G, CAR.ACURA_TLX_1G):
