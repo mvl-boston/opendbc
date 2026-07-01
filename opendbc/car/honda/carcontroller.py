4aaad400b068349883fbbd367f634d001c8f34ee
MVL <manuel@alumni.virginia.edu>
2026-07-01 00:32:42 -0400

    Update radar reference counter in CAN message creation

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -225,7 +225,7 @@ class CarController(CarControllerBase):
           self.radar_mux += 1
         can_sends.extend(hondacan.create_canfd_50hz_radar_messages(self.packer, self.CAN.pt, self.radar_mux))
       if self.frame % 20 == 0:
-        can_sends.extend(hondacan.create_canfd_5hz_radar_messages(self.packer, self.CAN.pt, self.radar_ref_counter))
+        can_sends.extend(hondacan.create_canfd_5hz_radar_messages(self.packer, self.CAN.pt, CS.radar_ref_counter))
 
     # Send steering command.
     can_sends.append(hondacan.create_steering_control(self.packer, self.CAN, apply_torque, CC.latActive, self.tja_control))
