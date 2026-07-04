488aa12c9436d2ca7fc23a23f19bfa29240d5d8e
MVL <manuel@alumni.virginia.edu>
2026-07-04 06:12:52 -0400

    Conditionally assign cp_radar based on carFingerprint

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -59,9 +59,10 @@ class CarState(CarStateBase):
   def update(self, can_parsers) -> structs.CarState:
     cp = can_parsers[Bus.pt]
     cp_cam = can_parsers[Bus.cam]
-    cp_radar = can_parsers[Bus.radar]
     if self.CP.enableBsm:
       cp_body = can_parsers[Bus.body]
+    if self.CP.carFingerprint in HONDA_BOSCH_CANFD:
+      cp_radar = can_parsers[Bus.radar]
 
     ret = structs.CarState()
 
