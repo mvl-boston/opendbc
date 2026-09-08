1b62b01d1fe4ddc78d2b6c84e8dee55d774e0998
MVL <manuel@alumni.virginia.edu>
2026-09-07 20:04:58 -0400

    Fix formatting of SOLID_LANES assignment

--- opendbc/car/honda/hondacan.py
+++ opendbc/car/honda/hondacan.py
@@ -184,7 +184,7 @@ def create_lkas_hud(packer, bus, CP, hud_control, lat_active, steering_available
     'LKAS_READY': 1,
     'LKAS_STATE_CHANGE': 1,
     'STEERING_REQUIRED': alert_steer_required,
-    'SOLID_LANES': lat_active and not steer_maxed
+    'SOLID_LANES': lat_active and not steer_maxed,
     'DASHED_LANES': dashed_lanes,
     'BEEP': 0,
   }
