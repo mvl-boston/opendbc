44f4b270ecdb8b7cda3434dc70dfad94f59e26c5
MVL <manuel@alumni.virginia.edu>
2026-09-07 23:42:04 -0400

    Simplify torque assignment in carcontroller.py
    Refactor actuator torque handling for Honda vehicles.

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -1205,20 +1205,18 @@ class CarController(CarControllerBase, MadsCarController, GasInterceptorCarContr
                                                                        self.last_button_frame, self.CAN))
 
     new_actuators = actuators.as_builder()
+    new_actuators.torque = self.last_torque
     if self.CP.flags & HondaFlags.BOSCH:
       new_actuators.speed = float(self.gasalpha)
       new_actuators.accel = self.accel
       new_actuators.gas = float(self.gasfactor)
       new_actuators.brake = float(self.windfactor)
+      new_actuators.torqueOutputCan = apply_torque
     else:
       new_actuators.speed = float(self.nidec_pid_factor)
       new_actuators.accel = float(self.accel)
       new_actuators.gas = float(self.average_factor)
       new_actuators.brake = float(self.sat_accel)
-    new_actuators.torque = self.last_torque
-    if self.CP.flags & HondaFlags.BOSCH:
-      new_actuators.torqueOutputCan = apply_torque
-    else:
       new_actuators.torqueOutputCan = float(self.speed_factors["low"])
 
     if self.frame % 6000 == 0:
