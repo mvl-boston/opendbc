daad4f631749ade9c46f40cda07fd484f977ccec
MVL <manuel@alumni.virginia.edu>
2026-09-08 01:22:55 -0400

    Update ACURA_TLX_1G CarSpecs with tire stiffness factor
    Added tire stiffness factor to ACURA_TLX_1G specs.

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -461,7 +461,7 @@ class CAR(Platforms):
   )
   ACURA_TLX_1G = HondaNidecPlatformConfig(
     [], # don't add to cardocs since custom steering board
-    CarSpecs(mass=3680 * CV.LB_TO_KG, wheelbase=2.78, steerRatio=17.0, centerToFrontRatio=0.40),  # as spec
+    CarSpecs(mass=3680 * CV.LB_TO_KG, wheelbase=2.78, steerRatio=17.0, centerToFrontRatio=0.40, tireStiffnessFactor=0.18),
     radar_dbc_dict('acura_mdx_3g_can_generated'),
     flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES | HondaFlags.HAS_ALL_DOOR_STATES,
   )
