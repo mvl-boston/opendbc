23affb3a8a5c736616dda9eeeb850f25981f4860
MVL <manuel@alumni.virginia.edu>
2026-09-08 00:39:51 -0400

    Fix syntax error in carstate.py conditional

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -180,7 +180,7 @@ class CarState(CarStateBase):
     if self.CP.openpilotLongitudinalControl:
       if self.CP.carFingerprint in HONDA_BOSCH_RADARLESS:
         ret.accFaulted = bool(cp.vl["CRUISE_FAULT_STATUS"]["CRUISE_FAULT"])
-      else if (self.CP.carFingerprint in (CAR.ACURA_MDX_4G, *HONDA_BOSCH_CANFD)) and (self.CP.flags & HondaFlags.BOSCH_ALT_BRAKE):
+      else if: (self.CP.carFingerprint in (CAR.ACURA_MDX_4G, *HONDA_BOSCH_CANFD)) and (self.CP.flags & HondaFlags.BOSCH_ALT_BRAKE):
         ret.accFaulted = bool(cp.vl["BRAKE_MODULE"]["CRUISE_FAULT"])
       else:
         ret.accFaulted = bool(cp.vl[self.brake_error_msg]["BRAKE_ERROR_1"] or cp.vl[self.brake_error_msg]["BRAKE_ERROR_2"])
